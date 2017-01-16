79441c7c61d1f599a2abe16b360c438f9874d75e
7coil <leondrolio@gmail.com>
2017-01-16 18:43:51 +0000

    Sexual Tension

--- bot.js
+++ bot.js
@@ -20,9 +20,9 @@ client.on('message', message => {
 
 	if (input[0] === '!help') {
 		var embed = new Discord.RichEmbed()
-			.setTitle('MSS')
-			.setAuthor('Help', 'http://moustacheminer.com/dickbutt.jpg')
-			.setColor(0x00AE86)
+			.setTitle('Help')
+			.setAuthor('MSS', 'http://moustacheminer.com/dickbutt.jpg')
+			.setColor("#00AE86")
 			.setDescription('Help can be found at our wiki page, or at the moustacheminer server services Discord server.')
 			.setFooter('moustacheminer.com server services', '')
 			.setTimestamp()
@@ -36,40 +36,47 @@ client.on('message', message => {
 	if (input[0] === '!play') {
 		let voiceChannel = message.member.voiceChannel;
 		if (!voiceChannel) {
-        	return richSend(message, "MSS", "!play", "Please be in a voice channel before using the !play command", "#FFFF00");
+        	return richSend(message, "!play", "Please be in a voice channel before using the !play command", "#FFFF00");
 	    }
 		
 		if (!input[1]) {
-        	return richSend(message, "MSS", "!play", "Please send a valid YouTube URL", "#FFFF00");
+        	return richSend(message, "!play", "Please send a valid YouTube URL", "#FFFF00");
 	    }
 		
 		voiceChannel.join()
 			.then(connnection => {
+				input[1] = input[1].replace(/%/g, "");
 				let stream = yt(input[1], {audioonly: true});
 				const dispatcher = connnection.playStream(stream);
 				dispatcher.on('end', () => {
 					voiceChannel.leave();
 				});
 			}).catch( error => {
-				richSend(message, "MSS", "!play", error.message, "#FF0000");
+				richSend(message, "!play", error.message, "#FF0000");
+				console.log(error);
 			});
 	}
 
 	if(input[0].toLowerCase().split('').filter(function(item, i, ar){ return ar.indexOf(item) === i; }).join('') === "cirletn"){
-		return richSend(message, "MSS", "Circletine", "CCCCCCCCCCCIIIIIIIIIIIIIIIIIRRRRRRRRRRRRRRRRRRRRRRRRCCCCCCCCCCCCCCCCCCCCCCLLLLLLLLLLLLLLLLLLEEEEEEEEEEEEEEETTTTTTTTTTTTTTTTTTIIIIIIIIIIIIIINNNNNNNNNNNNNNEEEEEEEEEEE", "#FFFFFF");
+		return richSend(message, "Circletine", "CCCCCCCCCCCIIIIIIIIIIIIIIIIIRRRRRRRRRRRRRRRRRRRRRRRRCCCCCCCCCCCCCCCCCCCCCCLLLLLLLLLLLLLLLLLLEEEEEEEEEEEEEEETTTTTTTTTTTTTTTTTTIIIIIIIIIIIIIINNNNNNNNNNNNNNEEEEEEEEEEE", "#FFFFFF");
+	}
+	
+	if(input[0] === 'sexual' && input[1] === 'tension') {
+		return richSend(message, "sexual tension", "sexual tension", "#FF9999", "https://cdn.discordapp.com/attachments/255362900187807744/269947744381042688/unknown.png");
 	}
 	
 });
 
-function richSend(message, title, subheading, description, colour) {
+function richSend(message, subheading, description, colour, img) {
 	var embed = new Discord.RichEmbed()
 		.setTitle(subheading)
-		.setAuthor(title, 'http://moustacheminer.com/dickbutt.jpg')
+		.setAuthor("MSS", 'http://moustacheminer.com/dickbutt.jpg')
 		.setColor(colour)
 		.setDescription(description)
 		.setFooter('moustacheminer.com server services', '')
 		.setTimestamp()
 		.setURL('http://moustacheminer.com/w/mss')
+		.setImage(img);
 
 	message.channel.sendEmbed(embed, "", { disableEveryone: true });
 }
