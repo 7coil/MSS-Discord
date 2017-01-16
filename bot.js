15065d155cb2ec2078dfe5e1bb6e9f6dce6f5ade
7coil <leondrolio@gmail.com>
2017-01-16 16:35:49 +0000

    Removed Reddit and RSS
    Also removed the footer icon for !help

--- bot.js
+++ bot.js
@@ -1,9 +1,9 @@
 // install ytdl-core before running this!
 // npm install --save ytdl-core
-const discord = require("discord.js");
+const Discord = require("discord.js");
 const feed = require("feed-read");
 const yt = require('ytdl-core');
-const client = new discord.Client();
+const client = new Discord.Client();
 const fs = require('fs');
 client.login("");
 
@@ -18,13 +18,14 @@ client.on('message', message => {
 	let input = message.content.replace( /\n/g, " " ).split(" ");
 	message.guild.member(client.user).setNickname('MSS')
 
+
 	if (input[0] === '!help') {
 		var embed = new Discord.RichEmbed()
 			.setTitle('MSS')
 				.setAuthor('Help', 'http://moustacheminer.com/dickbutt.jpg')
 				.setColor(0x00AE86)
 				.setDescription('Help can be found at our wiki page, or at the moustacheminer server services Discord server.')
-				.setFooter('moustacheminer server services', 'http://moustacheminer.com/dickbutt.jpg')
+				.setFooter('moustacheminer server services', '')
 				.setTimestamp()
 				.setURL('http://moustacheminer.com/w/mss')
 				.addField('Wiki', 'http://moustacheminer.com/w/mss')
@@ -45,37 +46,6 @@ client.on('message', message => {
 			dispatcher.on('end', () => {
 				voiceChannel.leave();
 			});
-		});
-	}
-	
-	if (input[0] === '!rss') {
-		if (!isInt(input[1])){
-			return message.reply("**Usage:** *!rss <integer number for article> <URL>*")
-		}
-		
-		feed(input[2], function(err, articles){
-			if (err) {
-				return message.reply(err);
-			}
-			message.channel.sendMessage("**" + articles[input[1]].title + "**\n" + articles[input[1]].content);
-		});
-	}
-	
-	if (input[0] === '!reddit') {
-		if (!isInt(input[2])){
-			input[2] = 1;
-		}
-		
-		feed("https://reddit.com/r/" + input[1] + "/.rss", function(err, articles){
-			if (err) {
-				return message.reply(err);
-			}
-			
-			if (!articles[input[2]].content) {
-				return message.reply("No articles found. Is this a valid subreddit?");
-			}
-			
-			message.channel.sendMessage("**" + articles[input[2]].title + "**\n" + articles[input[2]].content);
 		});
 	}
 
