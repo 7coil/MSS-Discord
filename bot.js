251c599c1312e69cbf5bfd9c6fda0f78647be465
7coil <leondrolio@gmail.com>
2017-02-05 00:13:43 +0000

    Added playlist feature

--- bot.js
+++ bot.js
@@ -41,6 +41,9 @@ const child_process = require('child_process');
 //Login to Discord's Bot API Service
 client.login(api);
 
+//Declare the playlist
+var playlist = [];
+
 //Set the running game and the avatar for the bot.
 client.on('ready', function() {
 	console.log("Successfully connected to Discord!");
@@ -70,73 +73,25 @@ client.on('message', message => {
 		} else if (input[0] === '!play') {
 			//Get the voice channel that it's going to play to.
 			let voiceChannel = message.member.voiceChannel;
-			
 			//Check if the user is inside a voice channel
 			if (!voiceChannel) {
-				return richSend(message, "!play", "Please be in a voice channel before using the !play command", "#FFFF00");
+				return richSend(message, input[0], "Please be in a voice channel before using the " + input[0] + " command", "#FFFF00");
 			}
 			
-			//Check if there is an input
-			//Purposefully split from the next if statement, if there is no input, the youtubeCheck() will fail
-			if (!input[1]) {
-				return richSend(message, "!play", "Please send a valid YouTube URL", "#FFFF00");
-			}
-			
-			//Check if the YouTube URL is valid
-			if (!youtubeCheck(input[1])) {
-				return richSend(message, "!play", "Please send a valid YouTube URL", "#FFFF00");
-			}
-			
-			//Get information about the YouTube URL
-			yt.getInfo(input[1], function(err, info) {
-				
-				//Exits if there is no video to be found
-				if (!info) {
-					return richSend(message, "!play", "Please send a valid YouTube URL", "#FF0000");
-				}
-				
-				//Exits if it's too long (that's what she said)
-				if (info["length_seconds"] > 3600 && !(message.channel.permissionsFor(message.member).hasPermission("ADMINISTRATOR") || message.author.id === "190519304972664832")) {
-					return richSend(message, "!play", "The duration of this video exceedes 1 hour.", "#FF0000");
-				}
-				
-				//Say what it's about to do
-				richSend(message, "Now playing:", info["title"], "#00FF00", info["thumbnail_url"], "https://youtube.com/watch?v=" + info["video_id"]);
-				
-				//Join the voiceChannel and blast the music.
-				voiceChannel.join()
-				.then(connnection => {
+			if (youtubeCheck(input[1])) {
+				yt.getInfo(input[1], function(err, info) {
+					if (!info) {
+						return richSend(message, "!play", "Please send a valid YouTube URL", "#FF0000");
+					}
 					
-					//Forgot what this does (Thumbs up for documentation
-					input[1] = input[1].replace(/%/g, "");
-					let stream = yt(input[1], {audioonly: true});
-					const dispatcher = connnection.playStream(stream);
-					dispatcher.on('end', () => {
-						//Leave after it's done
-						voiceChannel.leave();
-					});
-					
+					playlistAdd(message, "youtube", input[1], info["title"], info["thumbnail_url"]);
 				});
-			});
-
-		} else if (input[0] === '!stop') {
-			
-			//Check if the person has permission to stop the music
-			if (!(message.channel.permissionsFor(message.member).hasPermission("ADMINISTRATOR") || message.author.id === "190519304972664832")) {
-				return richSend(message, "!stop", "You do not have permission to stop the bot.", "#FF0000");
-			}
-			
-			//Get the voice channel
-			let voiceChannel = message.member.voiceChannel;
-			
-			//If the person is in the voice channel, stop the bot in that channel
-			if (voiceChannel) {
-				richSend(message, "!stop", "Stopped playing music in the channel.", "#00FF00");
-				return voiceChannel.leave();
+				
 			} else {
-				return richSend(message, "!stop", "There is no bot running in your current voice channel", "#FF0000");
+				console.log(input[1]);
 			}
-			
+		} else if (input[0] === '!stop') {
+			playlistClear(message);
 		} else if (input[0].toLowerCase().split('').filter(function(item, i, ar){ return ar.indexOf(item) === i; }).join('') === "cirletn"){
 			//Remove all repeated characters and check if it matches 'cirletn'
 			return richSend(message, "Circletine", "CCCCCCCCCCCIIIIIIIIIIIIIIIIIRRRRRRRRRRRRRRRRRRRRRRRRCCCCCCCCCCCCCCCCCCCCCCLLLLLLLLLLLLLLLLLLEEEEEEEEEEEEEEETTTTTTTTTTTTTTTTTTIIIIIIIIIIIIIINNNNNNNNNNNNNNEEEEEEEEEEE", "#FFFFFF");
@@ -150,23 +105,13 @@ client.on('message', message => {
 			
 			//Check if the user is inside a voice channel
 			if (!voiceChannel) {
-				return richSend(message, "!play", "Please be in a voice channel before using the !play command", "#FFFF00");
+				return richSend(message, "!dec", "Please be in a voice channel before using the !dec command", "#FFFF00");
 			}
 			
 			var file = Math.floor(Math.random() * 9999);
-			child_process.execFile('say', ['-w', 'C:\\MOUSTACHEMINER\\NodeJS\\MSS-Discord\\MSS-Discord\\DEC\\' + file + '.wav', dec], function(err, out, code) {
-				voiceChannel.join()
-				.then(connnection => {
-					let stream = fs.createReadStream('C:\\MOUSTACHEMINER\\NodeJS\\MSS-Discord\\MSS-Discord\\DEC\\' + file + '.wav');
-					const dispatcher = connnection.playStream(stream);
-					dispatcher.on('end', () => {
-						//Leave after it's done
-						voiceChannel.leave();
-					});
-				});
-			});
+			child_process.execFile('say', ['-w', 'C:\\MOUSTACHEMINER\\NodeJS\\MSS-Discord\\DEC\\' + file + '.wav', dec]);
+			playlistAdd(message, "file", 'C:\\MOUSTACHEMINER\\NodeJS\\MSS-Discord\\DEC\\' + file + '.wav', "DecTalk Input");
 		}
-		
 	} catch(err) {
 		//Catch those errors!
 		richSend(message, "Fatal error encountered.", err.stack + "\n\nPlease send a screenshot to the MSS Discord Server.\nhttps://discord.gg/ZW7GGGH", "#FF0000", "", "https://discord.gg/ZW7GGGH");
@@ -191,6 +136,68 @@ function richSend(message, subheading, description, colour, img, url) {
 		.setImage(img);
 
 	message.channel.sendEmbed(embed, "", { disableEveryone: true });
+}
+
+function playlistAdd(message, type, url, title, thumb_url) {
+	if (!thumb_url) {
+		thumb_url = "http://i48.tinypic.com/260v86c.png";
+	}
+	
+	richSend(message, "!play", "Added " + title + " to playlist", "#00FF00");
+	playlist[message.guild] = playlist[message.guild] || [];
+	playlist[message.guild].push(JSON.stringify({type: type, url: url, title: title, thumb_url: thumb_url}));
+	if (!message.member.voiceChannel.connection) {
+		playSound(message);
+	}
+}
+
+function playlistClear(message) {
+	//Check if the person has permission to stop the music
+	if (!(message.channel.permissionsFor(message.member).hasPermission("ADMINISTRATOR") || message.author.id === "190519304972664832")) {
+		return richSend(message, "!stop", "You do not have permission to stop the bot.", "#FF0000");
+	}
+
+	//Get the voice channel
+	let voiceChannel = message.member.voiceChannel;
+
+	//If the person is in the voice channel, stop the bot in that channel
+	if (voiceChannel && voiceChannel.connection) {
+		richSend(message, "!stop", "Stopped playing music in the channel.", "#00FF00");
+		return voiceChannel.leave();
+		playlist[message.guild] = [];
+	} else {
+		return richSend(message, "!stop", "There is no bot running in your current voice channel", "#FF0000");
+	}
+}
+
+function playSound(message) {
+	var voiceChannel = message.member.voiceChannel;
+	var current;
+	var stream;
+	
+	voiceChannel.join()
+	.then(connnection => {
+		var looper = function() {
+			if (playlist[message.guild].length > 0) {
+				current = JSON.parse(playlist[message.guild].shift());
+				richSend(message, "Now playing:", current["title"], "#00FF00", current["thumb_url"], current["url"]);
+
+				if (current["type"] === "youtube") {
+					stream = yt(current["url"], {audioonly: true});
+				} else if (current["type"] === "file") {
+					stream = fs.createReadStream(current["url"])
+				}
+				const dispatcher = connnection.playStream(stream);
+				dispatcher.on('end', () => {
+					looper();
+				});
+			} else {
+				voiceChannel.leave();
+				return;
+			}
+		}
+		looper();
+	});
 }
 
 /**
