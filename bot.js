1895153324f304b20552471fc538595e1017cae4
7coil <leondrolio@gmail.com>
2017-02-27 22:10:49 +0000

    Changed rulers to bombs

--- bot.js
+++ bot.js
@@ -108,12 +108,12 @@ client.on('message', message => {
 					if (error) {
 						console.log(error);
 						message.reply(error);
-						reactWith(message, false, "ruler");
+						reactWith(message, false, "bomb");
 						return false;
 					} else {
 						yt.getInfo(result["items"][0]["id"]["videoId"], function(err, info) {
 							if (!info) {
-								reactWith(message, false, "ruler");
+								reactWith(message, false, "bomb");
 								return false;
 							}
 							if (info["length_seconds"] > 3600 && !isAdmin(message)) return reactWith(message, false, "ruler");
