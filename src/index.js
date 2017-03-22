811d2304c9d788e4ccead21e682d522c76036154
7coil <leondrolio@gmail.com>
2017-03-22 18:18:47 +0000

    Include external file CONFIG.JSON

--- src/index.js
+++ src/index.js
@@ -1,19 +1,22 @@
-//Sets the version number for MSS-Discord
-const version = "Exports Branch";
-
 //A one dimensional array for each API key.
 //api[keyname]
 var api = [];
 
-//Get the Discord API Key.
-api["discord"] = process.argv[2];
+//Get the configuration file
+var config = require("./config.json");
+console.dir(config);
 
 //Declare the Discord and client constants used to set up the Discord bot
 const Discord = require("discord.js");
 const client = new Discord.Client();
-client.login(api["discord"]);
+client.login(config.API.discord);
 
 client.on('ready', function() {
 	console.log("Successfully connected to Discord!");
-	client.user.setGame("MSS Version: " + version);
+	client.user.setGame("MSS Version: " + config.MSS.version);
+});
+
+client.on('message', function(message) {
+	//if(message.content.startsWith(config.MSS.prefix);
+	//let input = message.content.replace (/\n/g, "").split(" ");
 });
