727bd6db9c4e1a5d8a431d29069bbd3b19371847
7coil <leondrolio@gmail.com>
2017-03-22 22:27:47 +0000

    Eval based on prefix

--- src/index.js
+++ src/index.js
@@ -18,8 +18,9 @@ client.on('ready', function() {
 client.on('message', function(message) {
 	if (!message.content.startsWith(config.MSS.prefix)) return false;
 	let input = message.content.replace (/\n/g, "").split(" ");
+	input[0] = input[0].substring(config.MSS.prefix.length);
 	
-	if (input[0] === "!!eval" && message.author.id === "190519304972664832") {
-		eval(message.content.substring(6));
+	if (input[0] === "eval" && message.author.id === "190519304972664832") {
+		eval(message.content.substring(config.MSS.prefix.length + input[0].length + 1));
 	}
 });
