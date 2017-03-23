1aca21c68b7e57f0f1dbd489aea07fb20881a110
7coil <leondrolio@gmail.com>
2017-03-23 23:47:09 +0000

    FIXED EMERGENCY EVAL FAULT

--- src/commands/eval.js
+++ src/commands/eval.js
@@ -2,7 +2,7 @@ const config = require("./../config.json");
 const MSS = require("./../functions/");
 
 module.exports = function(message) {
-	if(MSS.msg.isadmin(message)) {
+	if(message.author.id === config.MSS.sysadmin) {
 		let input = message.content.replace (/\n/g, "").split(" ");
 		input[0] = input[0].substring(config.MSS.prefix.length);
 		eval(message.content.substring(config.MSS.prefix.length + input[0].length + 1));
@@ -10,3 +10,4 @@ module.exports = function(message) {
 		MSS.msg.react(false, "X");
 	}
 }
+
