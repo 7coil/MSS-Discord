ca4287c49307ef1adbe8aee8d787bfa38c0f51af
7coil <leondrolio@gmail.com>
2017-03-23 23:42:45 +0000

    unquoting MSS.version

--- src/commands/help.js
+++ src/commands/help.js
@@ -4,7 +4,7 @@ module.exports = function help(message) {
 	message.reply("I sent a message via Direct Messaging with details enclosed.");
 	message.author.sendMessage(`
 **MSS, also known as Moustacheminer Server Services**
-_Version " + config.MSS.version + ", serving 2.6969696969696970 channels since 2016_
+_Version ` + config.MSS.version + `, serving 2.6969696969696970 channels since 2016_
 
 Commands
 \`\`\`
