79e78984863ace8b91846517fb7b41edc33db361
7coil <leondrolio@gmail.com>
2017-03-23 23:04:29 +0000

    added voicechannel vars

--- src/commands/stop.js
+++ src/commands/stop.js
@@ -2,6 +2,7 @@ const config = require("./../config.json");
 const MSS = require("./../functions/");
 
 module.exports = function stop(message) {
+	var voiceChannel = message.member.voiceChannel;
 	if (!voiceChannel || !voiceChannel.connection) {
 		//No bot in channel
 		return msg.react(message, false, "robot");
