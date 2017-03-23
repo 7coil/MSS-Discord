9379e5468f1c4fcf5bbb8913d1f0967e12cf5529
7coil <leondrolio@gmail.com>
2017-03-23 23:33:05 +0000

    lowercase X

--- src/functions/reaction/isadmin.js
+++ src/functions/reaction/isadmin.js
@@ -5,7 +5,7 @@ module.exports = function isadmin(user, channel) {
 	if (channel.permissionsFor(user).hasPermission("ADMINISTRATOR") || user.id === config.MSS.sysadmin) {
 		return true;
 	} else {
-		reactWith(message, false, "X");
+		reactWith(message, false, "x");
 		return false;
 	}
 }
