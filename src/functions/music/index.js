33b1ef101e327b9c84cd51a618044a72e7350197
7coil <leondrolio@gmail.com>
2017-03-23 22:50:27 +0000

    removed legacy rich send

--- src/functions/music/index.js
+++ src/functions/music/index.js
@@ -87,9 +87,9 @@ function stop(message) {
 
 function list(message) {
 	if (playlist[message.guild.id].length > 0) {
-		return richSend(message, "MSS Music Player", playlist[message.guild.id].map(function(sound){return JSON.parse(sound).title;}).join("\n"), "#00FF00");
+		return msg.rich(message, "MSS Music Player", playlist[message.guild.id].map(function(sound){return JSON.parse(sound).title;}).join("\n"), "#00FF00");
 	} else {
-		return richSend(message, "MSS Music Player", "There is no music remaining in the playlist.", "#FF0000");
+		return msg.rich(message, "MSS Music Player", "There is no music remaining in the playlist.", "#FF0000");
 	}
 }
 
