909bd06c487f81877a9028070c6c538c17ee9902
7coil <leondrolio@gmail.com>
2017-03-23 21:59:35 +0000

    Unexpected bracket

--- src/reactions/10145.js
+++ src/reactions/10145.js
@@ -4,4 +4,4 @@ const MSS = require("./../functions/");
 module.exports = function skip(messageReaction, user) {
 	if (!MSS.reactions.isadmin(user, messageReaction.message.channel)) return;
 	MSS.music.skip(messageReaction.message);
-});
+}
