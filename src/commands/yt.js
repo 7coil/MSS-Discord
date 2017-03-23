707d83ecd5e292f875f16a2dc856be023da23376
7coil <leondrolio@gmail.com>
2017-03-23 23:56:46 +0000

    YT success thingydoodad

--- src/commands/yt.js
+++ src/commands/yt.js
@@ -29,6 +29,7 @@ module.exports = function yt(message) {
 			MSS.msg.react(message, false, "X");
 			return false;
 		}  else {
+			MSS.msg.react(message, true);
 			MSS.music.add(message, "youtube", "https://youtube.com/watch?v=" + result["items"][0]["id"]["videoId"], result["items"][0]["snippet"]["title"], result["items"][0]["snippet"]["thumbnails"]["default"]["url"]);
 		}
 	});
