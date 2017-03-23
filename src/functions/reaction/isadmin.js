42de442db06b6a74cc4e5555ee7fbbb9ac27066f
7coil <leondrolio@gmail.com>
2017-03-23 22:02:36 +0000

    Had extra unnecessary input

--- src/functions/reaction/isadmin.js
+++ src/functions/reaction/isadmin.js
@@ -1,7 +1,7 @@
 const config = require("./../../config.json");
 
 module.exports = function isadmin(input, channel) {
-	if (input.channel.permissionsFor(input.member).hasPermission("ADMINISTRATOR") || input.author.id === config.MSS.sysadmin) {
+	if (channel.permissionsFor(input.member).hasPermission("ADMINISTRATOR") || input.author.id === config.MSS.sysadmin) {
 		return true;
 	} else {
 		reactWith(message, false, "bomb");
