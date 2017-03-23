9e7de36bf0941a68bbf06e49b2f014a5e8042d4c
7coil <leondrolio@gmail.com>
2017-03-23 18:53:52 +0000

    Forgot to edit out input[0]'s

--- src/commands/eval.js
+++ src/commands/eval.js
@@ -6,6 +6,7 @@ const MSS = require("./../functions/");
 module.exports = function(message) {
 	if(MSS.msg.isadmin(message)) {
 		let input = message.content.replace (/\n/g, "").split(" ");
+		input[0] = input[0].substring(config.MSS.prefix.length);
 		eval(message.content.substring(config.MSS.prefix.length + input[0].length + 1));
 	} else {
 		MSS.msg.react(false, "X");
