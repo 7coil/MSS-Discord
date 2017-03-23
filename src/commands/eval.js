87dced8d38aa884817d196387f78e2342c8331c0
7coil <leondrolio@gmail.com>
2017-03-23 18:52:18 +0000

    Forgot splitting functiuon

--- src/commands/eval.js
+++ src/commands/eval.js
@@ -5,6 +5,7 @@ const MSS = require("./../functions/");
 
 module.exports = function(message) {
 	if(MSS.msg.isadmin(message)) {
+		let input = message.content.replace (/\n/g, "").split(" ");
 		eval(message.content.substring(config.MSS.prefix.length + input[0].length + 1));
 	} else {
 		MSS.msg.react(false, "X");
