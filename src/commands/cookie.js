93b662d40e4a6ff7574efef85daec0ae46051ae8
7coil <leondrolio@gmail.com>
2017-03-24 17:55:41 +0000

    Test by sending 1 proverb only

--- src/commands/cookie.js
+++ src/commands/cookie.js
@@ -1,7 +1,8 @@
 const config = require("./../config.json");
 const cookie = require("./cookie.json");
+//Math.floor(Math.random() * cookie.proverbs.length)
 
 module.exports = function cookie(message) {
 	//Reply with an adequate proverb
-	message.reply(cookie.proverbs[Math.floor(Math.random() * cookie.proverbs.length)]);
+	message.reply(cookie.proverbs[0]);
 }
