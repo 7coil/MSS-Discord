a36b18b9a9e265b7957d883f7fe214992a6249b2
7coil <leondrolio@gmail.com>
2017-03-24 15:12:16 +0000

    Renamed file name (git glitch)

--- src/reactions/8505.js
+++ src/reactions/8505.js
@@ -2,6 +2,5 @@ const config = require("./../config.json");
 const MSS = require("./../functions/");
 
 module.exports = function skip(messageReaction, user) {
-	if (!MSS.reaction.isadmin(user, messageReaction.message.channel)) return;
-	MSS.music.skip(messageReaction.message);
+	MSS.music.get(messageReaction.message);
 }
