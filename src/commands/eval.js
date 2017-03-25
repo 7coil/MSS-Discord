12ed4ea8a0cad223e18715d6ba2f006aef107366
7coil <leondrolio@gmail.com>
2017-03-25 23:20:06 +0000

    Safety eval!

--- src/commands/eval.js
+++ src/commands/eval.js
@@ -5,7 +5,11 @@ module.exports = function(message) {
 	if(message.author.id === config.MSS.sysadmin) {
 		let input = message.content.replace (/\n/g, "").split(" ");
 		input[0] = input[0].substring(config.MSS.prefix.length);
-		eval(message.content.substring(config.MSS.prefix.length + input[0].length + 1));
+		try {
+			eval(message.content.substring(config.MSS.prefix.length + input[0].length + 1));
+		} catch(err) {
+			message.channel.sendMessage(err.stack);
+		}
 	} else {
 		MSS.msg.react(message, false, "X");
 	}
