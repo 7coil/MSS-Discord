50e6552408055827c921fe167877e5b36e24798a
7coil <leondrolio@gmail.com>
2017-03-25 23:51:24 +0000

    Too many nubs

--- src/commands/js.js
+++ src/commands/js.js
@@ -10,7 +10,7 @@ module.exports = function(message) {
 		input[0] = input[0].substring(config.MSS.prefix.length);
 		try {
 			s.run(message.content.substring(config.MSS.prefix.length + input[0].length + 1), function() {
-				message.reply(`\n\`\`\``+output.result+``\`\`\``);
+				message.reply(`\n\`\`\``+output.result+`\`\`\``);
 			});
 		} catch(err) {
 			message.reply("Something happened and an ACTUAL ERROR happened outside the sandbox.");
