55e3f7d7ca9f5c9eb929c8141e895f8e570b6f8c
7coil <leondrolio@gmail.com>
2017-03-26 21:47:10 +0100

    Not admin only

--- src/commands/js.js
+++ src/commands/js.js
@@ -6,38 +6,34 @@ const Discord = require("discord.js");
 module.exports = function(message) {
 	var s = new Sandbox;
 
-	if(message.author.id === config.MSS.sysadmin) {
-		let input = message.content.replace (/\n/g, " ").split(" ");
-		try {
-			let command = message.content.substring(input[0].length + 1);
-			console.log(command);
-			if (!command) return message.reply("No code was supplied.");
+	let input = message.content.replace (/\n/g, " ").split(" ");
+	try {
+		let command = message.content.substring(input[0].length + 1);
+		console.log(command);
+		if (!command) return message.reply("No code was supplied.");
 
-			s.run(command, function(output) {
-				var consoleout = output.console.length === 0 ? ["No output"] : output.console;
+		s.run(command, function(output) {
+			var consoleout = output.console.length === 0 ? ["No output"] : output.console;
 
-				var embed = new Discord.RichEmbed()
-					.setTitle("MSS-Discord JS Sandbox")
-					.setAuthor("js", "http://moustacheminer.com/mss.png")
-					.setColor("#00AE86")
-					.setDescription(command)
-					.setFooter("MSS-Discord, " + config.MSS.version, "")
-					.setTimestamp()
-					.setURL("http://moustacheminer.com/")
-					.addField("Result", output.result)
-					.addField("Console", consoleout);
+			var embed = new Discord.RichEmbed()
+				.setTitle("MSS-Discord JS Sandbox")
+				.setAuthor("js", "http://moustacheminer.com/mss.png")
+				.setColor("#00AE86")
+				.setDescription(command)
+				.setFooter("MSS-Discord, " + config.MSS.version, "")
+				.setTimestamp()
+				.setURL("http://moustacheminer.com/")
+				.addField("Result", output.result)
+				.addField("Console", consoleout);
 
-				console.log(output.result);
-				console.log(consoleout);
+			console.log(output.result);
+			console.log(consoleout);
 
-				message.channel.sendEmbed(embed, 'MSS-Discord JS Sandbox', { disableEveryone: true });
-			});
+			message.channel.sendEmbed(embed, 'MSS-Discord JS Sandbox', { disableEveryone: true });
+		});
 
-		} catch(err) {
-			message.reply("Something happened and an ACTUAL ERROR happened outside the sandbox.");
-		}
-	} else {
-		MSS.msg.react(message, false, "X");
+	} catch(err) {
+		message.reply("Something happened and an ACTUAL ERROR happened outside the sandbox.");
 	}
 }
 
