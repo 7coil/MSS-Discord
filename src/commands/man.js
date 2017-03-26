631a751d40bf76d8ddd7815d06625b9930f6f95b
7coil <leondrolio@gmail.com>
2017-03-26 00:06:53 +0000

    MORE LOGS

--- src/commands/man.js
+++ src/commands/man.js
@@ -22,6 +22,7 @@ module.exports = function manpages(message) {
 	if(!input[1]) {
 		var print = "Listing all valid commands - See info for commands by running " + config.MSS.prefix + "man <command>\n";
 		commands.forEach(function(element, index) {
+			console.log(index);
 			print.concat(" - " + index + "\n");
 		});
 		message.reply(print);
