165bf3fade77f6d05798aaed3d935ffd80b6936b
7coil <leondrolio@gmail.com>
2017-03-26 00:34:07 +0000

    Put both output and console

--- src/commands/js.js
+++ src/commands/js.js
@@ -6,11 +6,23 @@ module.exports = function(message) {
 	var s = new Sandbox;
 
 	if(message.author.id === config.MSS.sysadmin) {
-		let input = message.content.replace (/\n/g, "").split(" ");
+		let input = message.content.replace (/\n/g, " ").split(" ");
 		input[0] = input[0].substring(config.MSS.prefix.length);
 		try {
-			s.run(message.content.substring(config.MSS.prefix.length + input[0].length + 1), function(output) {
-				message.reply(`\n\`\`\``+output.result+`\`\`\``);
+			let command = message.content.substring(config.MSS.prefix.length + input[0].length + 1);
+			s.run(command, function(output) {
+				var embed = new Discord.RichEmbed()
+					.setTitle(commands[input[1]].meta.name)
+					.setAuthor("MSS-Discord JS Sandbox", "http://moustacheminer.com/mss.png")
+					.setColor("#00AE86")
+					.setDescription(command)
+					.setFooter("MSS-Discord, " + config.MSS.version, "")
+					.setTimestamp()
+					.setURL("http://moustacheminer.com/")
+					.addField("Result", output.result)
+					.addField("Console", output.console);
+
+				message.channel.sendEmbed(embed, 'MSS-Discord JS Sandbox', { disableEveryone: true });
 			});
 		} catch(err) {
 			message.reply("Something happened and an ACTUAL ERROR happened outside the sandbox.");
