760ea2a92488f0c69c454223424e0e4982c44968
7coil <leondrolio@gmail.com>
2017-03-27 18:12:31 +0100

    Remove react

--- src/functions/reaction/isadmin.js
+++ src/functions/reaction/isadmin.js
@@ -1,11 +1,9 @@
 const config = require("./../../config.json");
-const react = require("./../msg/react");
 
 module.exports = function isadmin(user, channel) {
 	if (channel.permissionsFor(user).hasPermission("ADMINISTRATOR") || user.id === config.MSS.sysadmin) {
 		return true;
 	} else {
-		react(message, false, "x");
 		return false;
 	}
 }
