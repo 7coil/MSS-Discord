a4e34bce3c9c6186fbca926ecc54ae90f5ed318a
7coil <leondrolio@gmail.com>
2017-03-27 16:51:27 +0100

    React with a unicode valid cookie

--- src/commands/cookie/index.js
+++ src/commands/cookie/index.js
@@ -4,5 +4,5 @@ const data = require("./data.json");
 module.exports = function(message) {
 	//Reply with an adequate cookie, then reply with a cookie reaction.
 	message.reply(data.cookies[Math.floor(Math.random() * data.cookies.length)]);
-	message.react(String.fromCodePoint(129376));
+	message.react(String.fromCodePoint(127850));
 }
