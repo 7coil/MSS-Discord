2c2d44cbc437e47f9ff397d95a5e9f71ef3e8ab6
7coil <leondrolio@gmail.com>
2017-03-27 16:49:54 +0100

    React with a cookie

--- src/commands/cookie/index.js
+++ src/commands/cookie/index.js
@@ -4,4 +4,5 @@ const data = require("./data.json");
 module.exports = function(message) {
 	//Reply with an adequate cookie, then reply with a cookie reaction.
 	message.reply(data.cookies[Math.floor(Math.random() * data.cookies.length)]);
+	message.react(String.fromCodePoint(129376));
 }
