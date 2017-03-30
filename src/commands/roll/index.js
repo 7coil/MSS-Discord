3b4e8c231c04fe972529b46c048538813c66bdeb
7coil <leondrolio@gmail.com>
2017-03-30 23:59:26 +0100

    Added a die dice die

---
+++ src/commands/roll/index.js
@@ -0,0 +1,61 @@
+const Discord = require("discord.js");
+
+module.exports = function(message, client) {
+	let input = message.content.replace(/\n/g, " ").split(" ");
+	let sides = 6;
+	let rolls = 1;
+	var result = [];
+	var output, sum;
+
+
+	if(input[1].startsWith("d")) {
+		//Check if it's trying to roll an "n" sided die.
+		sides = parseInt(input[1].substring(1).replace(/[^0-9]+/g, ''));
+	} else if (input[1]){
+		//Roll the dice the number of times stated
+		rolls = parseInt(input[1].replace(/[^0-9]+/g, ''));
+	}
+
+	if(input[2]) {
+		rolls = parseInt(input[1].replace(/[^0-9]+/g, ''));
+		sides = parseInt(input[1].replace(/[^0-9]+/g, ''));
+	}
+
+	if(!rolls || !sides || rolls < 1 || sides < 2) {
+		//You can't roll something with no die, no sides, less than 1 roll or with less than 2 sides.
+		MSS.msg.react(message, false, "x");
+	}
+
+	if(rolls == 1) {
+		output = "Rolled a " + sides + "sided die.";
+	} else {
+		//Rolled 6 6-sided dice.
+		output = "Rolled " + rolls + " " + sides + "-sided dice.";
+	}
+
+	for(i=0; i<rolls; i++) {
+		let value = randInt(sides);
+		sum += value;
+		output.push(value);
+	}
+
+
+
+
+	var embed = new Discord.RichEmbed()
+		.setTitle("MSS-Discord")
+		.setAuthor("DIE!", "http://moustacheminer.com/mss.png")
+		.setColor("#00AE86")
+		.setDescription(output)
+		.setFooter("MSS-Discord, " + config.MSS.version, "")
+		.setTimestamp()
+		.setURL("http://moustacheminer.com/")
+		.addField("Die output:", output)
+		.addField("Total", sum)
+
+	message.channel.sendEmbed(embed, "", { disableEveryone: true });
+}
+
+function randInt(d) {
+	Math.floor(Math.random() * d) + 1
+}
