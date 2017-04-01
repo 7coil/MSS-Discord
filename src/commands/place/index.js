97d111aa480c95bffd250e587877e462bf53a848
7coil <leondrolio@gmail.com>
2017-04-01 19:50:05 +0100

    Reuse old code

--- src/commands/place/index.js
+++ src/commands/place/index.js
@@ -13,87 +13,90 @@ module.exports = function place(message) {
 		}
 
 		var data = JSON.parse(body);
-		var response = "/r/place info for " + input[1] + "\n";
+		var reply = "";
 		var overflow = false;
 
-		data.info.forEach(function(element) {
-			var colour;
-			switch(element.colour) {
-				case 1:
-					colour = "Light Grey";
-					break;
-				case 2:
-					colour = "Grey";
-					break;
-				case 3:
-					colour = "Black";
-					break;
-				case 4:
-					colour = "Pink";
-					break;
-				case 5:
-					colour = "Red";
-					break;
-				case 6:
-					colour = "Orange";
-					break;
-				case 7:
-					colour = "Brown";
-					break;
-				case 8:
-					colour = "Yellow";
-					break;
-				case 9:
-					colour = "Lime";
-					break;
-				case 10:
-					colour = "Green";
-					break;
-				case 11:
-					colour = "Cyan";
-					break;
-				case 12:
-					colour = "Blue";
-					break;
-				case 13:
-					colour = "Dark Blue";
-					break;
-				case 14:
-					colour = "Magenta";
-					break;
-				case 15:
-					colour = "Purple";
-					break;
-				default:
-					colour = "Invalid Colour ID";
-					break;
-			}
+		if(!data.info) {
+			reply += "The user has not placed any dots down, or does not exist.";
+		} else {
+			data.info.forEach(function(element) {
+				var colour;
+				switch(parseInt(element.colour)) {
+					case 1:
+						colour = "Light Grey";
+						break;
+					case 2:
+						colour = "Grey";
+						break;
+					case 3:
+						colour = "Black";
+						break;
+					case 4:
+						colour = "Pink";
+						break;
+					case 5:
+						colour = "Red";
+						break;
+					case 6:
+						colour = "Orange";
+						break;
+					case 7:
+						colour = "Brown";
+						break;
+					case 8:
+						colour = "Yellow";
+						break;
+					case 9:
+						colour = "Lime";
+						break;
+					case 10:
+						colour = "Green";
+						break;
+					case 11:
+						colour = "Cyan";
+						break;
+					case 12:
+						colour = "Blue";
+						break;
+					case 13:
+						colour = "Dark Blue";
+						break;
+					case 14:
+						colour = "Magenta";
+						break;
+					case 15:
+						colour = "Purple";
+						break;
+					default:
+						colour = "Invalid Colour ID";
+						break;
+				}
 
-			if (response < 1800) {
-				response += "Coloured (" + element.x + "," + element.y + ") " + colour + " at " + new Date(parseInt(element.time)) + "\n";
-			} else {
-				overflow = true;
-			}
-		});
+				if(reply.length < 1900) {
+					reply += "Coloured (" + element.x + "," + element.y + ") " + colour + " at " + new Date(parseInt(element.time)) + "\n";
+				} else {
+					overflow = true;
+				}
+
+			});
+		}
 
 		//Yes, false is a string. I'm so sorry
 		if(data.error != "false") {
 			message.reply(data.error);
 		} else {
-			if(overflow) {
-				response += "The response is too large to display here: Please click the URL in the embed to view the rest.";
-			}
-
 			var embed = new Discord.RichEmbed()
 				.setTitle(input[1])
-				.setAuthor("/r/place", "http://moustacheminer.com/mss.png")
+				.setAuthor("/r/place info for " + input[1], "http://moustacheminer.com/mss.png")
 				.setColor("#00AE86")
-				.setDescription(response)
+				.setDescription(reply)
 				.setFooter("MSS-Discord, " + config.MSS.version, "")
 				.setTimestamp()
 				.setURL("http://moustacheminer.com/place/?username=" + user);
 
-
+			if(overflow) {
+				embed.addField("Overflow Error", "Please go to this URL to view the rest of the log. http://moustacheminer.com/place/?username=" + user);
+			}
 
 			message.channel.sendEmbed(embed, "", { disableEveryone: true });
 		}
