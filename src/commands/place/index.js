5a343715053cb340ad992e04b420170b269b4825
7coil <leondrolio@gmail.com>
2017-04-03 16:10:28 +0100

    Info for each colour instead of everything

--- src/commands/place/index.js
+++ src/commands/place/index.js
@@ -13,75 +13,36 @@ module.exports = function place(message) {
 		}
 
 		var data = JSON.parse(body);
-		var reply = "";
-		var overflow = false;
+		var reply;
+		var colour = [];
 
 		if(!data.info) {
 			reply += "The user has not placed any dots down, or does not exist.";
 		} else {
 			data.info.forEach(function(element) {
-				var colour;
-				switch(parseInt(element.colour)) {
-					case 0:
-						colour = "White";
-						break;
-					case 1:
-						colour = "Light Grey";
-						break;
-					case 2:
-						colour = "Grey";
-						break;
-					case 3:
-						colour = "Black";
-						break;
-					case 4:
-						colour = "Pink";
-						break;
-					case 5:
-						colour = "Red";
-						break;
-					case 6:
-						colour = "Orange";
-						break;
-					case 7:
-						colour = "Brown";
-						break;
-					case 8:
-						colour = "Yellow";
-						break;
-					case 9:
-						colour = "Lime";
-						break;
-					case 10:
-						colour = "Green";
-						break;
-					case 11:
-						colour = "Cyan";
-						break;
-					case 12:
-						colour = "Blue";
-						break;
-					case 13:
-						colour = "Dark Blue";
-						break;
-					case 14:
-						colour = "Magenta";
-						break;
-					case 15:
-						colour = "Purple";
-						break;
-					default:
-						colour = "Invalid Colour ID";
-						break;
-				}
-
-				if(reply.length < 1900) {
-					reply += "Coloured (" + element.x + "," + element.y + ") " + colour + " at " + new Date(parseInt(element.time)) + "\n";
-				} else {
-					overflow = true;
-				}
-
+				colour[element.colour]++
 			});
+
+			reply = \
+			`Info for ` + user + `\n
+				White      | ` + colour[0]  + ` | ` + (colour[0]  / data.length).toFixed(2) + `%
+				Light Grey | ` + colour[1]  + ` | ` + (colour[1]  / data.length).toFixed(2) + `%
+				Grey       | ` + colour[2]  + ` | ` + (colour[2]  / data.length).toFixed(2) + `%
+				Black      | ` + colour[3]  + ` | ` + (colour[3]  / data.length).toFixed(2) + `%
+				Pink       | ` + colour[4]  + ` | ` + (colour[4]  / data.length).toFixed(2) + `%
+				Red        | ` + colour[5]  + ` | ` + (colour[5]  / data.length).toFixed(2) + `%
+				Orange     | ` + colour[6]  + ` | ` + (colour[6]  / data.length).toFixed(2) + `%
+				Brown      | ` + colour[7]  + ` | ` + (colour[7]  / data.length).toFixed(2) + `%
+				Yellow     | ` + colour[8]  + ` | ` + (colour[8]  / data.length).toFixed(2) + `%
+				Lime       | ` + colour[9]  + ` | ` + (colour[9]  / data.length).toFixed(2) + `%
+				Green      | ` + colour[10] + ` | ` + (colour[10] / data.length).toFixed(2) + `%
+				Cyan       | ` + colour[11] + ` | ` + (colour[11] / data.length).toFixed(2) + `%
+				Blue       | ` + colour[12] + ` | ` + (colour[12] / data.length).toFixed(2) + `%
+				Dark Blue  | ` + colour[13] + ` | ` + (colour[13] / data.length).toFixed(2) + `%
+				Magenta    | ` + colour[14] + ` | ` + (colour[14] / data.length).toFixed(2) + `%
+				Purple     | ` + colour[15] + ` | ` + (colour[15] / data.length).toFixed(2) + `%
+				-------------------------
+				Total      | ` + data.length;
 		}
 
 		//Yes, false is a string. I'm so sorry
@@ -95,11 +56,8 @@ module.exports = function place(message) {
 				.setDescription(reply)
 				.setFooter("MSS-Discord, " + config.MSS.version, "")
 				.setTimestamp()
-				.setURL("http://moustacheminer.com/place/?username=" + user);
-
-			if(overflow) {
-				embed.addField("Overflow Error", "Please go to this URL to view the rest of the log. http://moustacheminer.com/place/?username=" + user);
-			}
+				.setURL("http://moustacheminer.com/place/?username=" + user).
+				.addField("Powered by /r/PlaceDevs", "Please go to this URL to view the rest of the log. http://moustacheminer.com/place/?username=" + user);
 
 			message.channel.sendEmbed(embed, "", { disableEveryone: true });
 		}
