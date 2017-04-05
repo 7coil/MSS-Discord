229c0c57b63d9b3718830f03c630a0fa599d7622
7coil <leondrolio@gmail.com>
2017-04-06 00:27:12 +0100

    Check if less than 1 or non-existant

--- src/functions/music/index.js
+++ src/functions/music/index.js
@@ -104,10 +104,11 @@ function stop(message) {
 }
 
 function list(message) {
-	if (playlist[message.guild.id].length > 0) {
-		return msg.rich(message, "MSS Music Player", playlist[message.guild.id].map(function(sound){return JSON.parse(sound).title;}).join("\n"), "#00FF00");
-	} else {
+	//Check if it does not exist, or has less than 1 item remaining.
+	if (!playlist[message.guild.id] || playlist[message.guild.id].length < 1) {
 		return msg.rich(message, "MSS Music Player", "There is no music remaining in the playlist.", "#FF0000");
+	} else {
+		return msg.rich(message, "MSS Music Player", playlist[message.guild.id].map(function(sound){return JSON.parse(sound).title;}).join("\n"), "#00FF00");
 	}
 }
 
