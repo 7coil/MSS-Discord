3f450325080ec40ee0e63040b2196b31a3c8446c
7coil <leondrolio@gmail.com>
2017-04-07 18:21:45 +0100

    Added better embed for HELP

--- src/commands/help/index.js
+++ src/commands/help/index.js
@@ -1,16 +1,13 @@
 const config = require("./../../config.json");
 
 module.exports = function help(message) {
-	message.reply("I sent a message via Direct Messaging with details enclosed.");
-	message.author.sendMessage(`
-**MSS, also known as Moustacheminer Server Services**
-_Version ` + config.MSS.version + `, serving 2.6969696969696970 channels since 2016_
+	var embed = new Discord.RichEmbed()
+		.setFooter("MSS-Discord, " + config.MSS.version, "")
+		.setTimestamp()
+		.addField("MSS", config.MSS.version)
+		.addField("Commands", "For a list of commands, please run the '" + config.MSS.prefix + "man' command.\nFor help on a specific command, please run '" + config.MSS.prefix + "man <command>'.")
+		.addField("Links", "[Discord Server](http://moustacheminer.com/r/invite-server)\n[Invite Bot](http://moustacheminer.com/r/invite-bot)\n[GitHub](http://moustacheminer.com/r/github-bot)")
+		.addField("Licence", "This software is released under the MIT Licence.");
 
-For a list of commands, please run the '` + config.MSS.prefix + `man' command.
-For help on a specific command, please run '` + config.MSS.prefix + `man <command>'.
-
-Discord Server: http://moustacheminer.com/r/invite-server
-Invite Bot: http://moustacheminer.com/r/invite-bot
-GitHub: http://moustacheminer.com/r/github-bot
-`);
+	message.channel.sendEmbed(embed, "", { disableEveryone: true });
 }
