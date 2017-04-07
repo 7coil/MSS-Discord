84994e1516d4b00fc92504705f58393ebda4a292
7coil <leondrolio@gmail.com>
2017-04-07 21:31:28 +0100

    reply

--- src/commands/ow/index.js
+++ src/commands/ow/index.js
@@ -5,10 +5,10 @@ module.exports = function manpages(message) {
 	var owUrl = "http://127.0.0.1:3000/profile/" + input[1] + "/" + input[2] + "/" + input[3];
 	request(owUrl, function (error, response, body) {
 		if (response.statusCode === 404) {
-			return sendMessage("No player tag was found.");
+			return message.reply("No player tag was found.");
 		} else if (response.statusCode === 200){
 			var owData = JSON.parse(body);
-			return sendMessage(owData["username"] + " Ranked #" + owData["competitive"]["rank"] + " W/L " + owData["games"]["competitive"]["wins"] + "/" + owData["games"]["competitive"]["lost"]);
+			return message.reply(owData["username"] + " Ranked #" + owData["competitive"]["rank"] + " W/L " + owData["games"]["competitive"]["wins"] + "/" + owData["games"]["competitive"]["lost"]);
 		}
 	});
 }
