d491a02f81f24c394cfa35f0832dbc614dce1537
7coil <leondrolio@gmail.com>
2017-04-11 17:56:53 +0100

    Starting to XML these commands

--- src/commands/cookie/index.js
+++ src/commands/cookie/index.js
@@ -1,8 +1,14 @@
 const config = require("./../../config.json");
 const data = require("./data.json");
+const meta = require("./meta.json");
 
 module.exports = function(message) {
 	//Reply with an adequate cookie, then reply with a cookie reaction.
-	message.reply(data.cookies[Math.floor(Math.random() * data.cookies.length)]);
-	message.react(String.fromCodePoint(127850));
+	message.channel.sendmessage(`\`\`\`xml
+<response>
+	<name>` + meta.meta.name + `</name>
+	<to>` + message.author.tag + `</to>
+	<reply>` + data.cookies[Math.floor(Math.random() * data.cookies.length)] + `</reply>
+</response>
+\`\`\``);
 }
