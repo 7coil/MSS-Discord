19a34f07b971e4768f8890648ea70712b8413b81
7coil <leondrolio@gmail.com>
2017-04-11 20:19:02 +0100

    XML /r/place

--- src/commands/place/index.js
+++ src/commands/place/index.js
@@ -1,6 +1,7 @@
 const Discord = require("discord.js");
 const config = require("./../../config.json");
 const MSS = require("./../../functions/");
+const meta = require("./meta.json");
 const request = require("request");
 
 module.exports = function place(message) {
@@ -23,47 +24,48 @@ module.exports = function place(message) {
 		if(!data.info) {
 			reply += "The user has not placed any dots down, or does not exist.";
 		} else {
-			data.info.forEach(function(element) {
-				colour[element.colour]++
-			});
 
-			reply = `Info for ` + user + `
-\`\`\`
-White      | ` + colour[0]  + ` | ` + ((colour[0]  / data.info.length)*100).toFixed(2) + `%
-Light Grey | ` + colour[1]  + ` | ` + ((colour[1]  / data.info.length)*100).toFixed(2) + `%
-Grey       | ` + colour[2]  + ` | ` + ((colour[2]  / data.info.length)*100).toFixed(2) + `%
-Black      | ` + colour[3]  + ` | ` + ((colour[3]  / data.info.length)*100).toFixed(2) + `%
-Pink       | ` + colour[4]  + ` | ` + ((colour[4]  / data.info.length)*100).toFixed(2) + `%
-Red        | ` + colour[5]  + ` | ` + ((colour[5]  / data.info.length)*100).toFixed(2) + `%
-Orange     | ` + colour[6]  + ` | ` + ((colour[6]  / data.info.length)*100).toFixed(2) + `%
-Brown      | ` + colour[7]  + ` | ` + ((colour[7]  / data.info.length)*100).toFixed(2) + `%
-Yellow     | ` + colour[8]  + ` | ` + ((colour[8]  / data.info.length)*100).toFixed(2) + `%
-Lime       | ` + colour[9]  + ` | ` + ((colour[9]  / data.info.length)*100).toFixed(2) + `%
-Green      | ` + colour[10] + ` | ` + ((colour[10] / data.info.length)*100).toFixed(2) + `%
-Cyan       | ` + colour[11] + ` | ` + ((colour[11] / data.info.length)*100).toFixed(2) + `%
-Blue       | ` + colour[12] + ` | ` + ((colour[12] / data.info.length)*100).toFixed(2) + `%
-Dark Blue  | ` + colour[13] + ` | ` + ((colour[13] / data.info.length)*100).toFixed(2) + `%
-Magenta    | ` + colour[14] + ` | ` + ((colour[14] / data.info.length)*100).toFixed(2) + `%
-Purple     | ` + colour[15] + ` | ` + ((colour[15] / data.info.length)*100).toFixed(2) + `%
------------------------------
-Total      | ` + data.info.length + `\`\`\``;
-		}
 
 		//Yes, false is a string. I'm so sorry
 		if(data.error != "false") {
 			message.reply(data.error);
 		} else {
-			var embed = new Discord.RichEmbed()
-				.setTitle(input[1])
-				.setAuthor("/r/place info for " + input[1], "http://moustacheminer.com/mss.png")
-				.setColor("#00AE86")
-				.setDescription(reply)
-				.setFooter("MSS-Discord, " + config.MSS.version, "")
-				.setTimestamp()
-				.setURL("http://moustacheminer.com/place/?username=" + user)
-				.addField("Powered by /r/PlaceDevs", "Please go to this URL to view the rest of the log. http://moustacheminer.com/place/?username=" + user);
+			data.info.forEach(function(element) {
+				colour[element.colour]++
+			});
 
-			message.channel.sendEmbed(embed, "", { disableEveryone: true });
+			reply = {
+				response: {
+					name: meta.meta.name,
+					to: message.author.username,
+					error: false,
+					output: {
+						name: user,
+						colour: {
+							0: colour[0],
+							1: colour[1],
+							2: colour[2],
+							3: colour[3],
+							4: colour[4],
+							5: colour[5],
+							6: colour[6],
+							7: colour[7],
+							8: colour[8],
+							9: colour[9],
+							10: colour[10],
+							11: colour[11],
+							12: colour[12],
+							13: colour[13],
+							14: colour[14],
+							15: colour[15],
+						}
+						count: data.info.length
+						url: "http://moustacheminer.com/place/?username=" + user
+					}
+				}
+			}
+
+			MSS.msg.xml(message, reply);
 		}
 	});
 }
