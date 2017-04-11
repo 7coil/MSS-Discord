65ca231e584fc10755c5765fdcf8036360bbb8c3
7coil <leondrolio@gmail.com>
2017-04-11 20:13:28 +0100

    Change error message

--- src/commands/ow/index.js
+++ src/commands/ow/index.js
@@ -13,44 +13,44 @@ module.exports = function manpages(message) {
 				name: meta.meta.name,
 				to: message.author.username,
 				error: true,
-				output: "The player was not found."
+				output: "Not all variables were provided"
 			}
 		}
 
 		MSS.msg.xml(message, reply);
 		return false;
-	} else {
-		//Make the input the correct format
-		input[1] = input[1].toLowerCase();
-		input[2] = input[2].toLowerCase();
-		input[3] = input[3].replace("#", "-");
-		var owUrl = "http://127.0.0.1:3000/profile/" + input[1] + "/" + input[2] + "/" + input[3];
-		request(owUrl, function (error, response, body) {
-			if (response.statusCode === 404) {
-				reply = {
-					response: {
-						name: meta.meta.name,
-						to: message.author.username,
-						error: true,
-						output: "The player was not found."
-					}
-				}
-
-				MSS.msg.xml(message, reply);
-			} else if (response.statusCode === 200){
-				var owData = JSON.parse(body);
-
-				reply = {
-					response: {
-						name: meta.meta.name,
-						to: message.author.username,
-						error: false,
-						output: owData
-					}
-				}
-
-				MSS.msg.xml(message, reply);
-			}
-		});
 	}
+
+	//Make the input the correct format
+	input[1] = input[1].toLowerCase();
+	input[2] = input[2].toLowerCase();
+	input[3] = input[3].replace("#", "-");
+	var owUrl = "http://127.0.0.1:3000/profile/" + input[1] + "/" + input[2] + "/" + input[3];
+	request(owUrl, function (error, response, body) {
+		if (response.statusCode === 404) {
+			reply = {
+				response: {
+					name: meta.meta.name,
+					to: message.author.username,
+					error: true,
+					output: "The player was not found."
+				}
+			}
+
+			MSS.msg.xml(message, reply);
+		} else if (response.statusCode === 200){
+			var owData = JSON.parse(body);
+
+			reply = {
+				response: {
+					name: meta.meta.name,
+					to: message.author.username,
+					error: false,
+					output: owData
+				}
+			}
+
+			MSS.msg.xml(message, reply);
+		}
+	});
 }
