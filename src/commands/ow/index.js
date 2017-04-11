158879e489aa893a12ba4ebec56a8c24737ab8a0
7coil <leondrolio@gmail.com>
2017-04-11 20:11:20 +0100

    Check if the three vars exist first

--- src/commands/ow/index.js
+++ src/commands/ow/index.js
@@ -1,9 +1,25 @@
 const Discord = require("discord.js");
 const config = require("./../../config.json");
 const request = require('request');
+const MSS = require("./../../functions/");
+const meta = require("./meta.json");
 
 module.exports = function manpages(message) {
 	let input = message.content.replace(/\n/g, " ").split(" ");
+
+	if(input[1] && input[2] && input[3]) {
+		reply = {
+			response: {
+				name: meta.meta.name,
+				to: message.author.username,
+				error: true,
+				output: "The player was not found."
+			}
+		}
+
+		MSS.msg.xml(message, reply);
+		return false;
+	}
 
 	//Make the input the correct format
 	input[1] = input[1].toLowerCase();
