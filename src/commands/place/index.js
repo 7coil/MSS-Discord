09ecb6daa978bb00cc97be02c2f01c13e666f665
7coil <leondrolio@gmail.com>
2017-04-11 20:21:43 +0100

    Better errors

--- src/commands/place/index.js
+++ src/commands/place/index.js
@@ -22,50 +22,67 @@ module.exports = function place(message) {
 		}
 
 		if(!data.info) {
-			reply += "The user has not placed any dots down, or does not exist.";
-		} else {
-
-
-		//Yes, false is a string. I'm so sorry
-		if(data.error != "false") {
-			message.reply(data.error);
-		} else {
-			data.info.forEach(function(element) {
-				colour[element.colour]++
-			});
-
 			reply = {
 				response: {
 					name: meta.meta.name,
 					to: message.author.username,
-					error: false,
-					output: {
-						name: user,
-						colour: {
-							0: colour[0],
-							1: colour[1],
-							2: colour[2],
-							3: colour[3],
-							4: colour[4],
-							5: colour[5],
-							6: colour[6],
-							7: colour[7],
-							8: colour[8],
-							9: colour[9],
-							10: colour[10],
-							11: colour[11],
-							12: colour[12],
-							13: colour[13],
-							14: colour[14],
-							15: colour[15]
-						},
-						count: data.info.length,
-						url: "http://moustacheminer.com/place/?username=" + user
-					}
+					error: true,
+					output: "The user does not exist, or has not placed any pixels down."
 				}
 			}
 
 			MSS.msg.xml(message, reply);
+		} else {
+			//Yes, false is a string. I'm so sorry
+			if(data.error != "false") {
+				reply = {
+					response: {
+						name: meta.meta.name,
+						to: message.author.username,
+						error: true,
+						output: "An error occured with the server."
+					}
+				}
+
+				MSS.msg.xml(message, reply);
+			} else {
+				data.info.forEach(function(element) {
+					colour[element.colour]++
+				});
+
+				reply = {
+					response: {
+						name: meta.meta.name,
+						to: message.author.username,
+						error: false,
+						output: {
+							name: user,
+							colour: {
+								0: colour[0],
+								1: colour[1],
+								2: colour[2],
+								3: colour[3],
+								4: colour[4],
+								5: colour[5],
+								6: colour[6],
+								7: colour[7],
+								8: colour[8],
+								9: colour[9],
+								10: colour[10],
+								11: colour[11],
+								12: colour[12],
+								13: colour[13],
+								14: colour[14],
+								15: colour[15]
+							},
+							count: data.info.length,
+							url: "http://moustacheminer.com/place/?username=" + user
+						}
+					}
+				}
+
+				MSS.msg.xml(message, reply);
+			}
 		}
 	});
 }
