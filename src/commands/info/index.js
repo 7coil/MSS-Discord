f9a97fdbc7bb171333407e330bc7b56972158c6a
7coil <leondrolio@gmail.com>
2017-04-11 13:10:54 +0100

    Made guilds appear instead of shards

--- src/commands/info/index.js
+++ src/commands/info/index.js
@@ -2,13 +2,15 @@ const Discord = require("discord.js");
 const config = require("./../../config.json");
 const os = require('os');
 
+//Non-changing statistics
 //  (x64) AMD Athlon (x3) Something @ 3ghz (clocked at ????MHz)
 var hardwareinfo = "[CPU] (" + os.arch() + ") " + os.cpus()[0]["model"] + " (clocked at " + os.cpus()[0]["speed"] + "MHz)";
 var softwareinfo = "[" + os.type() + "] " + os.release() + "\n[Hostname] " + os.hostname();
 
 module.exports = function(message) {
-	var pinginfo = message.client.ping + "ms";
 
+	//Realtime statistics
+	var pinginfo = message.client.ping + "ms";
 	var guildcount = message.client.guilds.size;
 	var shardcount = config.MSS.shards;
 	var shardguildratio = (message.client.guilds.size / config.MSS.shards).toFixed(2);
@@ -21,15 +23,14 @@ module.exports = function(message) {
 		.addField("Ping", pinginfo, true)
 		.addField("Node.js", process.version, true)
 		.addField("Uptime", process.uptime(), true)
-		.addField("Shard", shardinfo, true)
+		.addField("Guilds", guildcount, true)
 		.addField("PID", process.pid, true)
 		.addField("Hardware", hardwareinfo)
 		.addField("Software", softwareinfo)
 		.addField("Licence", "This software is released under the MIT Licence.");
 
-	var shardinfo;
 	if(shardcount) {
-		embed.addField("Guilds", guildcount, true)
+		embed.addField("Shard", shardinfo, true)
 			.addField("Shards", shardcount, true)
 			.addField("Shard/Guild Ratio", shardguildratio, true);
 	}
