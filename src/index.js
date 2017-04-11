a7754f82e356579670f32dee3f920a2b8a8fdd43
7coil <leondrolio@gmail.com>
2017-04-11 13:01:39 +0100

    Make shards automagic

--- src/index.js
+++ src/index.js
@@ -2,4 +2,5 @@ const Discord = require('discord.js');
 const config = require('./config.json');
 const manager = new Discord.ShardingManager('./bot.js');
 
-manager.spawn(config.MSS.shards);
+//Spawn with a relevant number of shards automatically
+manager.spawn();
