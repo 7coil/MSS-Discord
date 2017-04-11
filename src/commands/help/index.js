b72a9f65a0bf6a7ee4a2ec2ddbdbe0fd8edad0f8
7coil <leondrolio@gmail.com>
2017-04-11 19:18:12 +0100

    XMLified Info

--- src/commands/help/index.js
+++ src/commands/help/index.js
@@ -4,12 +4,22 @@ const meta = require("./meta.json");
 const MSS = require("./../../functions/");
 
 module.exports = function help(message) {
-	reply = [
-		"For a list of commands, run man, with option of 'all'.",
-		"For help on a specific command, run man, with the option of your choice.",
-		"This version of MSS-Discord (XML-Discord) is by 7coil, at moustacheminer.com",
-		"This software is released under the MIT Licence"
-	];
 
-	MSS.msg.xml(message, meta, reply);
+	reply = {
+		response: {
+			name: meta.meta.name,
+			to: message.author.username,
+			error: false,
+			output: {
+				help: [
+					"For a list of commands, run man, with option of 'all'.",
+					"For help on a specific command, run man, with the option of your choice.",
+					"This version of MSS-Discord (XML-Discord) is by 7coil, at moustacheminer.com",
+					"This software is released under the MIT Licence"
+				]
+			}
+		}
+	}
+
+	MSS.msg.xml(message, help);
 }
--- src/commands/info/index.js
+++ src/commands/info/index.js
@@ -4,36 +4,38 @@ const os = require('os');
 const meta = require("./meta.json");
 const MSS = require("./../../functions/");
 
-//Non-changing statistics
-//  (x64) AMD Athlon (x3) Something @ 3ghz (clocked at ????MHz)
-var hardwareinfo = "[CPU] (" + os.arch() + ") " + os.cpus()[0]["model"] + " (clocked at " + os.cpus()[0]["speed"] + "MHz)";
-var softwareinfo = "[" + os.type() + "] " + os.release() + "\n[Hostname] " + os.hostname();
-
 module.exports = function(message) {
 	var reply = [];
 
 	//Realtime statistics
 	var pinginfo = message.client.ping + "ms";
 	var guildcount = message.client.guilds.size;
-	var shardcount = config.MSS.shards;
-	var shardguildratio = (message.client.guilds.size / config.MSS.shards).toFixed(2);
-	var shardinfo = message.client.shard.id;
 
-	reply.push("MSS: " + config.MSS.version);
-	reply.push("Ping: " + pinginfo);
-	reply.push("Node.js: " + process.version);
-	reply.push("Uptime: " + process.uptime());
-	reply.push("Guilds: " + guildcount);
-	reply.push("PID: " + process.pid);
-	reply.push("Hardware: " + hardwareinfo);
-	reply.push("Software: " + softwareinfo);
-	reply.push("Licence: This software is released under the MIT Licence.");
-
-	if(shardcount) {
-		reply.push("Shard: " + shardinfo);
-		reply.push("Shards: " + shardcount);
-		reply.push("Shard/Guild Ratio: " + shardguildratio);
+	reply = {
+		response: {
+			name: meta.meta.name,
+			to: message.author.username,
+			error: false,
+			output: {
+				xml: config.MSS.version,
+				ping: pinginfo,
+				node: process.version,
+				uptime: process.uptime(),
+				guilds: guildcount,
+				pid: process.pid,
+				hardware: {
+					arch: os.arch(),
+					model: os.cpus()[0]["model"],
+					clock: os.cpus()[0]["speed"] + "MHz"
+				},
+				software: {
+					type: os.type(),
+					release: os.release(),
+					hostname: os.hostname()
+				}
+			}
+		}
 	}
 
-	MSS.msg.xml(message, meta, reply);
+	MSS.msg.xml(message, help);
 }
