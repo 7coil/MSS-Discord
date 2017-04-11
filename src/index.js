73f420d634399f26a329fd66f1afc6221e1aeb8b
7coil <leondrolio@gmail.com>
2017-04-11 13:05:41 +0100

    Add token in sharding manager

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 const Discord = require('discord.js');
 const config = require('./config.json');
-const manager = new Discord.ShardingManager('./bot.js');
+const API = require("./api.json");
+const manager = new Discord.ShardingManager('./bot.js', {
+	token: API.discord
+});
 
 //Spawn with a relevant number of shards automatically
 manager.spawn();
