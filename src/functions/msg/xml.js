a565162257f7fd8ff07d6904971b39cdc2d3f195
7coil <leondrolio@gmail.com>
2017-04-11 18:08:27 +0100

    Put bracket in wrong place

--- src/functions/msg/xml.js
+++ src/functions/msg/xml.js
@@ -10,18 +10,18 @@ module.exports = function reactWith(message, meta, output, error) {
 	if(output.length > 0 && output.isArray) {
 		output.forEach(function(element) {
 			reply += "    <output>" + element + "</output>\n"
-		}
+		});
 	} else if (output) {
 		reply += "    <output>" + output + "</output>\n"
-	});
+	}
 
 	if(error.length > 0 && output.isArray) {
 		output.forEach(function(element) {
 			reply += "    <error>" + element + "</error>\n"
-		}
+		});
 	} else if (error) {
 		reply += "    <error>" + error + "</error>\n"
-	});
+	}
 
 	reply += "</response>"
 
