40681da6b0d1974e99b8275debcb19b22dd6b7b9
7coil <leondrolio@gmail.com>
2017-04-11 18:57:21 +0100

    remove var

--- src/functions/msg/xml.js
+++ src/functions/msg/xml.js
@@ -5,8 +5,8 @@ module.exports = function xmlReply(input) {
 	var builder = new xml2js.Builder();
 
 	var reply = "```xml";
-	var reply += builder.buildObject(input);
-	var reply += "```"
+	reply += builder.buildObject(input);
+	reply += "```"
 
 	message.channel.sendMessage(reply);
 }
