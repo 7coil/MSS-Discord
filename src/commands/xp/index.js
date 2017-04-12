aef20fa9b0f4858f581e7da680a6126897fe24d5
7coil <leondrolio@gmail.com>
2017-04-12 10:47:57 +0100

    Forgot a function

--- src/commands/xp/index.js
+++ src/commands/xp/index.js
@@ -3,7 +3,7 @@ const command = "vncsnapshot 192.168.0.3:5 export.jpg";
 const Discord = require("discord.js");
 
 module.exports = function screenshot(message) {
-	exec(command, (error, stdout, stderr) {
+	exec(command, function(error, stdout, stderr) {
 		 message.sendFile('./export.jpg', '.jpg', stdout);
 	});
 }
