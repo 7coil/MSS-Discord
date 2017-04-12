dd42b43f5907ef0e770f637dd2659a8fee265883
7coil <leondrolio@gmail.com>
2017-04-12 13:28:24 +0100

    Changed screen

--- src/commands/xp/index.js
+++ src/commands/xp/index.js
@@ -1,5 +1,5 @@
 const exec = require('child_process').exec;
-const command = "vncsnapshot 192.168.0.3:5 export.jpg";
+const command = "vncsnapshot 192.168.0.3:3 export.jpg";
 const Discord = require("discord.js");
 
 module.exports = function screenshot(message) {
