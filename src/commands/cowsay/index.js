3a98af576901caaec6a4e866f4a52830218c2d0a
7coil <leondrolio@gmail.com>
2017-04-18 17:36:50 +0100

    Insert cowsay

---
+++ src/commands/cowsay/index.js
@@ -0,0 +1,16 @@
+const exec = require('child_process').execFile;
+
+module.exports = function screenshot(message) {
+	let input = message.content.replace(/\n/g, "").split(" ");
+
+	//Get everything after cowsay
+	input.shift();
+
+	if(!input[0]) {
+		input[0] = "Have you mooed today?";
+	}
+
+	execFile('cowsay', input, function(error, result) {
+		 message.channel.sendMessage('```\n' + result + '\n```');
+	});
+}
