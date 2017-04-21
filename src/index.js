ac1a085c1ddef63bc70afa0150f1fcf0056c1b63
7coil <leondrolio@gmail.com>
2017-04-22 00:16:18 +0100

    Selfbot measures

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 const Discord = require('discord.js');
 const config = require('./config.json');
 const API = require("./api.json");
-const manager = new Discord.ShardingManager('./bot.js', {
-	token: API.discord
-});
+const exec = require("child_process").exec;
 
-//Spawn with a relevant number of shards automatically
-manager.spawn();
+if(!config.MSS.selfbot) {
+	//Do this if it's a real bot
+	const manager = new Discord.ShardingManager('./bot.js', {
+		token: API.discord
+	});
+
+	//Spawn with a relevant number of shards automatically
+	manager.spawn();
+} else {
+	//It's a selfbot. Just start it up.
+	exec('node ./bot.js');
+}
