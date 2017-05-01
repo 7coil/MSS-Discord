377ee170d63f6666eeb637dd893a6832e5a1682c
lepon01 <leondrolio@gmail.com>
2017-05-01 18:22:31 +0100

    It's further down

--- src/commands/ban/types/owner/index.js
+++ src/commands/ban/types/owner/index.js
@@ -1,4 +1,4 @@
-const MSS = require("./../../functions/");
+const MSS = require("./../../../../functions/");
 
 module.exports = (message, connection) => {
 	const input = message.content.replace(/\n/g, ' ').split(' ');
--- src/commands/ban/types/verify/index.js
+++ src/commands/ban/types/verify/index.js
@@ -1,4 +1,4 @@
-const MSS = require("./../../functions/");
+const MSS = require("./../../../../functions/");
 
 module.exports = (message, connection) => {
 	MSS.msg.react(message, true);
