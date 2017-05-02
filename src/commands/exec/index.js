abef2729590ac43be4c82c1501ae64a57b5168c8
lepon01 <leondrolio@gmail.com>
2017-05-02 22:20:55 +0100

    replace pull with exec

---
+++ src/commands/exec/index.js
@@ -0,0 +1,34 @@
+const config = require("./../../config.json");
+const exec = require('child_process').exec;
+
+module.exports = function screenshot(message) {
+	let input = message.content.replace(/\n/g, "").split(" ");
+
+	if(message.author.id === config.MSS.sysadmin) {
+
+		input.shift();
+		input.shift();
+
+		if(!input[0]) {
+			message.channel.sendMessage("```\nNo input detected\n```");
+			return false;
+		}
+
+		let command = input.splice(2).join(" ");
+		let output = "```\n";
+
+		exec(command, {cwd: "/home/mss/"} ,(error, stdout, stderr) => {
+			if(stdout){
+				output += stdout + "\n";
+			}
+
+			if(stderr){
+				output += stderr + "\n";
+			}
+
+			output += "```";
+
+			message.channel.sendMessage(output);
+		});
+	}
+}
