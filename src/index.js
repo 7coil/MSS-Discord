424dac8bc831266eb71d520f0aee0a6596ec789e
lepon01 <leondrolio@gmail.com>
2017-05-05 16:03:20 +0100

    Update version

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const API = require("./api.json");
 const exec = require("child_process").exec;
 
 if(!config.MSS.selfbot) {
+
 	//Do this if it's a real bot
 	const manager = new Discord.ShardingManager('./bot.js', {
 		token: API.discord
@@ -11,7 +12,8 @@ if(!config.MSS.selfbot) {
 
 	//Spawn with a relevant number of shards automatically
 	manager.spawn();
+
 } else {
-	//It's a selfbot. Just start it up.
-	exec('node ./bot.js');
+	console.log("You are running in SELFBOT mode. This is not an officially supported mode, and we don't recommend you do this.");
+	console.log("To continue, run > node bot.js <");
 }
