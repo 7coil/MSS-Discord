847f80e31f444f87051e86a8507018790609be5d
lepon01 <leondrolio@gmail.com>
2017-05-08 19:20:01 +0100

    duplicate some moo code because it broke

--- src/commands/git/index.js
+++ src/commands/git/index.js
@@ -1,33 +1,25 @@
 const config = require("./../../config.json");
-const exec = require('child_process').execFile;
+const execFile = require('child_process').execFile;
 
 module.exports = function screenshot(message) {
 	let input = message.content.replace(/\n/g, "").split(" ");
 
 	if(message.author.id === config.MSS.sysadmin) {
-		if(!input[0]) {
+		if(!input[2]) {
 			message.channel.sendMessage("```\nNo input detected\n```");
 			return false;
 		}
 
-		//Shift!
+		//Get everything after cowsay
 		input.shift();
 		input.shift();
 
-		let output = "```\n";
+		if(!input[0]) {
+			input[0] = "Have you git pulled today?";
+		}
 
-		exec('git', input, (error, stdout, stderr) => {
-			if(stdout){
-				output += stdout + "\n";
-			}
-
-			if(stderr){
-				output += stderr + "\n";
-			}
-
-			output += "```";
-
-			message.channel.sendMessage(output);
+		execFile('git', input, function(error, result) {
+			 message.channel.sendMessage('```\n' + result + '\n```');
 		});
 	}
 }
