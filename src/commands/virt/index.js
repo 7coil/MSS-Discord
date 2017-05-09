8092f798fc1e11e2759982a406d0561115d3ff37
lepon01 <leondrolio@gmail.com>
2017-05-09 22:38:34 +0100

    Remove MSS commands

--- src/commands/virt/index.js
+++
@@ -1,3 +0,0 @@
-module.exports = function(message) {
-	message.reply("Project VM has ended! Why not try out @DiscordPlays, our next collaborative project? Type `@DiscordPlays help` for more info");
-}
