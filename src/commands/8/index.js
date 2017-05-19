88c3a858d102f65333c59b4331c22961193cd653
lepon01 <leondrolio@gmail.com>
2017-05-19 17:47:10 +0100

    Add 8 ball

---
+++ src/commands/8/index.js
@@ -0,0 +1,8 @@
+const config = require("./../../config.json");
+const data = require("./data.json");
+
+module.exports = function(message) {
+	//Reply with an adequate cookie, then reply with a cookie reaction.
+	message.reply(data.messages[Math.floor(Math.random() * data.cookies.length)]);
+	message.react(String.fromCodePoint(127850));
+}
