28018368836e1a6886275018cf437aedc94077a7
lepon01 <leondrolio@gmail.com>
2017-05-23 17:21:59 +0100

    rename data to meta

--- src/commands/help/index.js
+++ src/commands/help/index.js
@@ -7,9 +7,9 @@ module.exports = function help(message) {
 		.setFooter("MSS-Discord, " + config.MSS.version, "")
 		.setTimestamp()
 		.addField("MSS", config.MSS.version)
-		.addField(data[message.data.lang] && data[message.data.lang].commands || "commands", data[message.data.lang] && data[message.data.lang].paragraph1 || "paragraph1")
-		.addField(data[message.data.lang] && data[message.data.lang].links || "links", data[message.data.lang] && data[message.data.lang].paragraph2 || "paragraph2")
-		.addField(data[message.data.lang] && data[message.data.lang].licence || "licence", data[message.data.lang] && data[message.data.lang].paragraph3 || "paragraph3");
+		.addField(meta[message.data.lang] && meta[message.data.lang].commands || "commands",	meta[message.data.lang] && meta[message.data.lang].paragraph1 || "paragraph1")
+		.addField(meta[message.data.lang] && meta[message.data.lang].links || "links",			meta[message.data.lang] && meta[message.data.lang].paragraph2 || "paragraph2")
+		.addField(meta[message.data.lang] && meta[message.data.lang].licence || "licence",		meta[message.data.lang] && meta[message.data.lang].paragraph3 || "paragraph3");
 
 	message.channel.send("", { embed: embed, disableEveryone: true });
 }
