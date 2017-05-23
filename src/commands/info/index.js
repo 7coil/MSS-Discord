bb8084353f97eb9bd9294afca7839769b8f00cb3
lepon01 <leondrolio@gmail.com>
2017-05-23 17:49:29 +0100

    Add locale to info

--- src/commands/info/index.js
+++ src/commands/info/index.js
@@ -1,34 +1,33 @@
 const Discord = require("discord.js");
 const config = require("./../../config.json");
+const meta = require("./meta.json");
 const os = require('os');
-
-//Non-changing statistics
-//  (x64) AMD Athlon (x3) Something @ 3ghz (clocked at ????MHz)
-var hardwareinfo = "[CPU] (" + os.arch() + ") " + os.cpus()[0]["model"] + " (clocked at " + os.cpus()[0]["speed"] + "MHz)";
-var softwareinfo = "[" + os.type() + "] " + os.release() + "\n[Hostname] " + os.hostname();
 
 module.exports = function(message) {
 
 	//Realtime statistics
 	var pinginfo = message.client.ping.toFixed(2) + "ms";
 	var guildcount = message.client.guilds.size;
+	var hardwareinfo = "[CPU] (" + os.arch() + ") " + os.cpus()[0]["model"] + " (" + os.cpus()[0]["speed"] + "MHz)";
+	var softwareinfo = `[${os.type()}] ${os.release()}\n[${meta[message.data.lang] && meta[message.data.lang].hostname || "hostname"}] ${os.hostname()}`;
+	var softwareinfo = "[" + os.type() + "] " + os.release() + "\n[Hostname] " + os.hostname();
 
 	var embed = new Discord.RichEmbed()
 		.setFooter("MSS-Discord, " + config.MSS.version, "")
 		.setTimestamp()
 		.addField("MSS", config.MSS.version, true)
-		.addField("Ping", pinginfo, true)
+		.addField(meta[message.data.lang] && meta[message.data.lang].ping || "ping", pinginfo, true)
 		.addField("Node.js", process.version, true)
-		.addField("Uptime", process.uptime(), true)
-		.addField("Guilds", guildcount, true)
+		.addField(meta[message.data.lang] && meta[message.data.lang].uptime || "uptime", process.uptime(), true)
+		.addField(meta[message.data.lang] && meta[message.data.lang].guilds || "guilds", guildcount, true)
 		.addField("PID", process.pid, true)
-		.addField("Hardware", hardwareinfo)
-		.addField("Software", softwareinfo)
+		.addField(meta[message.data.lang] && meta[message.data.lang].hardware || "hardware", hardwareinfo)
+		.addField(meta[message.data.lang] && meta[message.data.lang].software || "software", softwareinfo)
 
 	if(config.MSS.shards) {
-		embed.addField("Shard", message.client.shard.id, true)
-			.addField("Shards", config.MSS.shards, true)
-			.addField("Shard/Guild Ratio", (message.client.guilds.size / config.MSS.shards).toFixed(2), true);
+		embed.addField(meta[message.data.lang] && meta[message.data.lang].shard || "shard", message.client.shard.id, true)
+			.addField(meta[message.data.lang] && meta[message.data.lang].shards || "shards", config.MSS.shards, true)
+			.addField(meta[message.data.lang] && meta[message.data.lang].ratio || "ratio", (message.client.guilds.size / config.MSS.shards).toFixed(2), true);
 	}
 
 	message.channel.send("", { embed: embed, disableEveryone: true });
