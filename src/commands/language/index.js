404dcf00b263018c62d3b77b61aec839ebbd6171
lepon01 <leondrolio@gmail.com>
2017-05-23 15:54:49 +0100

    check if not undefined first

--- src/commands/language/index.js
+++ src/commands/language/index.js
@@ -7,9 +7,13 @@ module.exports = function (message) {
 	let input = message.content.replace(/\n/g, "").split(" ");
 	console.dir(input);
 
-	if(!data.languages[input[2]]) return message.reply(meta[message.data.lang].err || "ERR");
+	if(!data.languages[input[2]]) return message.reply(typeof meta[input[2]] != "undefined" && meta[input[2]].err ||  || "ERR");
 
-	message.reply(meta[input[2]].set || "SET");
+	//ONLY EXCEPTION FOR LANGUAGE
+	//Gets the language code from the message,
+	//as it is the one that was just set by
+	//the (l)user
+	message.reply(typeof meta[input[2]] != "undefined" && meta[input[2]].set || "SET");
 
 	r.table("users").insert({
 		id: message.member.id,
