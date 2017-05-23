b06b92e484dfa5623e78c8d2d342a956339e29f1
lepon01 <leondrolio@gmail.com>
2017-05-23 16:56:40 +0100

    Add pirate locale for 8 ball

--- src/commands/8/index.js
+++ src/commands/8/index.js
@@ -3,6 +3,6 @@ const data = require("./data.json");
 
 module.exports = function(message) {
 	//Reply with an adequate 8 ball
-	message.reply(data.messages[Math.floor(Math.random() * data.messages.length)]);
+	message.reply(data[message.data.lang][Math.floor(Math.random() * data[message.data.lang].length)] || "Translate me!");
 	message.react(String.fromCodePoint(127921));
 }
