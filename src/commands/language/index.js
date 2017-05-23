bf4c00df5a00b899889f5667f2e8c06990ebd91b
lepon01 <leondrolio@gmail.com>
2017-05-23 14:09:10 +0100

    Put inside if

--- src/commands/language/index.js
+++ src/commands/language/index.js
@@ -11,12 +11,12 @@ module.exports = function (message) {
 	console.log(`Lang`);
 
 	//Check if the input is a valid language
-	if(!lang[input[2]]) return false;
+	if(lang[input[2]]) {
+		console.log(`Setting language of ${message.member.tag} to ${input[2]}`);
 
-	console.log(`Setting language of ${message.member.tag} to ${input[2]}`);
-
-	r.table("users").insert(
-		{"id": message.member.id, "lang": input[2]},
-		conflict = "replace"
-	).run(message.guild.rethonk);
+		r.table("users").insert(
+			{"id": message.member.id, "lang": input[2]},
+			conflict = "replace"
+		).run(message.guild.rethonk);
+	}
 }
