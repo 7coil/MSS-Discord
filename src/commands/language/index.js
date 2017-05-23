6e9905cd7acebb364a4f61532192217928e15dc9
lepon01 <leondrolio@gmail.com>
2017-05-23 16:14:40 +0100

    Print list of langs

--- src/commands/language/index.js
+++ src/commands/language/index.js
@@ -3,17 +3,32 @@ const meta = require("./meta.json");
 const data = require("./data.json");
 
 module.exports = function (message) {
+
+	//If there is no input, send a list of languages
+	if (!input[2]) {
+		let print = typeof meta[message.data.lang] != "undefined" && meta[message.data.lang].success_found || "message_choose_lang";
+		let langs = [];
+		print += "\n```\n"
+		data.languages.forEach(function(item) {
+			langs.push(item[message.data.lang]);
+		});
+
+		print += langs.join(`, `);
+		print += "```";
+		return message.reply(print);
+	}
+
 	//Split message into keywords
 	let input = message.content.replace(/\n/g, "").split(" ");
 	console.dir(input);
 
-	if(!data.languages[input[2]]) return message.reply(typeof meta[input[2]] != "undefined" && meta[input[2]].err || "ERR");
+	if(!data.languages[message.data.lang]) return message.reply(typeof meta[message.data.lang] != "undefined" && meta[message.data.lang].err_not_found || "err_not_found");
 
 	//ONLY EXCEPTION FOR LANGUAGE
 	//Gets the language code from the message,
 	//as it is the one that was just set by
 	//the (l)user
-	message.reply(typeof meta[input[2]] != "undefined" && meta[input[2]].set || "SET");
+	message.reply(typeof meta[input[2]] != "undefined" && meta[input[2]].success_found || "success_found");
 
 	r.table("users").insert({
 		id: message.member.id,
