ab6b4261ade86278c4d26cda4e245a54e4c941cd
lepon01 <leondrolio@gmail.com>
2017-05-23 14:33:31 +0100

    why is the if not working

--- src/commands/language/index.js
+++ src/commands/language/index.js
@@ -8,11 +8,11 @@ module.exports = function (message) {
 	//Split message into keywords
 	let input = message.content.replace(/\n/g, "").split(" ");
 
-	console.log(`Lang`);
+	console.log(`Setting language of ${message.member.tag} to ${input[2]}`);
 
 	//Check if the input is a valid language
 	if(lang[input[2]]) {
-		console.log(`Setting language of ${message.member.tag} to ${input[2]}`);
+		console.log(`Successfully found language`);
 
 		r.table("users").insert(
 			{"id": message.member.id, "lang": input[2]},
