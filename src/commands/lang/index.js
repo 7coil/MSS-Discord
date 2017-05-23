8e346ad6e238b1833749f53d55c9f769c61fe259
lepon01 <leondrolio@gmail.com>
2017-05-23 21:20:10 +0100

    check if keys match user's current language

--- src/commands/lang/index.js
+++ src/commands/lang/index.js
@@ -24,7 +24,7 @@ module.exports = function (message) {
 	input[2] = input[2].substr(0,2)
 	console.dir(input);
 
-	if(!data.languages.some((element)=>{return element == message.data.lang})) return message.reply(meta[message.data.lang] && meta[message.data.lang].err_not_found || "err_not_found");
+	if(!Object.keys(data.languages).some((element)=>{return element == message.data.lang})) return message.reply(meta[message.data.lang] && meta[message.data.lang].err_not_found || "err_not_found");
 
 	//ONLY EXCEPTION FOR LANGUAGE
 	//Gets the language code from the message,
