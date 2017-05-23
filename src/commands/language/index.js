0b3933ecbae7fa18196cafea27280a87f4a8b5cc
lepon01 <leondrolio@gmail.com>
2017-05-23 14:05:15 +0100

    log

--- src/commands/language/index.js
+++ src/commands/language/index.js
@@ -11,6 +11,8 @@ module.exports = function list(message) {
 	//Check if the input is a valid language
 	if(!lang[input[2]]) return false;
 
+	console.log(`Setting language of ${message.member.tag} to ${input[2]}`);
+
 	r.table("users").insert(
 		{"id": message.member.id, "lang": input[2]},
 		conflict = "replace"
