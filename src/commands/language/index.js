dfe036e7ec91c22c56a02b39291eb6786f942936
lepon01 <leondrolio@gmail.com>
2017-05-23 16:19:38 +0100

    iterate keys of languages

--- src/commands/language/index.js
+++ src/commands/language/index.js
@@ -11,7 +11,7 @@ module.exports = function (message) {
 		let print = typeof meta[message.data.lang] != "undefined" && meta[message.data.lang].success_found || "message_choose_lang";
 		let langs = [];
 		print += "\n```\n"
-		data.languages.forEach(function(item) {
+		Object.keys(data.languages).forEach(function(item) {
 			langs.push(item[message.data.lang]);
 		});
 
