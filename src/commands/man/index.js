b99063863c3a1cb69840280ed524589ab46fc3a7
lepon01 <leondrolio@gmail.com>
2017-05-24 18:59:38 +0100

    Add pirate to ALL commands

--- src/commands/man/index.js
+++ src/commands/man/index.js
@@ -3,10 +3,11 @@ const MSS = require("./../../functions/");
 const config = require("./../../config.json");
 const fs = require("fs");
 var commands = [];
-var print = "What manual page do you want?\n`@MSS man <command>`\n```\n";
 
-//Get all .json files in this directory to read the man data.
+//Construct the "full" list
 fs.readdir("./commands/", function(err, items) {
+	var print = "\n`@MSS man <command>`\n```\n";
+
 	items.forEach(function(item) {
 		var file = item.replace(/['"]+/g, "");
 		commands[file] = require(`./../${file}/meta.json`);
@@ -21,27 +22,27 @@ module.exports = function manpages(message) {
 
 	//Return the usage of the man command if no attributes were given
 	if(!input[2]) {
-		message.reply(print);
+		message.reply(`${meta[message.data.lang] && meta[message.data.lang].message_choose_man || "message_choose_man"} ${print}`);
 		return false;
 	}
 
 	//Return if it doesn't exist
 	if (!commands[input[2]]) {
 		MSS.msg.react(message, false, "link");
-		message.reply("No manual entry for " + input[1]);
+		message.reply(`${meta[message.data.lang] && meta[message.data.lang].err_not_found || "err_not_found"} ${input[1]}`);
 		return false;
 	}
 
 	var embed = new Discord.RichEmbed()
-		.setTitle(commands[input[2]].meta.name)
-		.setAuthor("MSS Man Pages", "http://moustacheminer.com/mss.png")
+		.setTitle(commands[input[2]][message.data.lang]name)
+		.setAuthor(meta[message.data.lang] && meta[message.data.lang].message_man_pages || "message_man_pages", "http://moustacheminer.com/img/mss.png")
 		.setColor("#00AE86")
-		.setDescription(commands[input[2]].meta.description)
+		.setDescription(commands[input[2]][message.data.lang].description)
 		.setFooter("MSS-Discord, " + config.MSS.version, "")
 		.setTimestamp()
-		.setURL(commands[input[2]].meta.url);
+		.setURL(commands[input[2]][message.data.lang].url);
 
-	commands[input[2]].meta.examples.forEach(function(element) {
+	commands[input[2]][message.data.lang].examples.forEach(function(element) {
 		embed.addField(input[2] + " " + element.var, element.description);
 	});
 
