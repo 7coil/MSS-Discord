32b8bdc676e3db85032cebf49a780bd0b5513a70
lepon01 <leondrolio@gmail.com>
2017-05-25 23:04:41 +0100

    Add prune command

--- src/commands/prune/index.js
+++ src/commands/prune/index.js
@@ -4,16 +4,14 @@ const meta = require("./meta.json");
 
 module.exports = function screenshot(message) {
 	if(MSS.msg.isadmin(message)) {
-		let fetch = "";
-		let del = "";
 
 		message.channel.fetchMessages({limit: 100})
-			.then(messages => {
-				console.log(messages.size);
-				message.channel.bulkDelete(messages.filter((message) => {return message.author.id === client.user.id}))
-			})
-			.catch((err)=>{fetch = err.fetch;});
+			.then((messages)=>{
+				let msgs = messages.filter((msg) => {
+					return msg.author.id === message.client.user.id;
+				});
 
-		if(fetch || del) message.channel.send(`${meta[message.data.lang] && meta[message.data.lang].err_prune || "err_prune"}\n${fetch}\n${del}`)
+				message.channel.bulkDelete(msgs);
+			});
 	}
 }
