ec93e776446a310459a89a53c2232774e409e33c
lepon01 <leondrolio@gmail.com>
2017-05-25 23:32:46 +0100

    Flip on correct js

--- src/functions/msg/isadmin.js
+++ src/functions/msg/isadmin.js
@@ -1,11 +1,9 @@
 const config = require("./../../config.json");
-const react = require("./react");
 
 module.exports = function isadmin(message) {
-	if (message.channel.permissionsFor(message.member).hasPermission("ADMINISTRATOR") || message.author.id === config.MSS.sysadmin) {
+	if (message.author.id === config.MSS.sysadmin || message.channel.permissionsFor(message.member).hasPermission("ADMINISTRATOR")) {
 		return true;
 	} else {
-		react(message, false, "x");
 		return false;
 	}
 }
