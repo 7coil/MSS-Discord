dad50161c5d393c679c20f07b5a978b47c9e9978
lepon01 <leondrolio@gmail.com>
2017-05-26 18:40:27 +0100

    whoops

--- src/commands/lang/index.js
+++ src/commands/lang/index.js
@@ -12,7 +12,7 @@ module.exports = function (message) {
 		let langs = [];
 		print += "\n```\n"
 		Object.keys(data.languages).forEach(function(item) {
-			langs.push(`${item} | ${data.languages[message.data.lang][message.data.lang]} (${data.languages[item][message.data.lang]})`);
+			langs.push(`${item} | ${data.languages[item][item]} (${data.languages[item][message.data.lang]})`);
 		});
 
 		print += langs.join("\n");
