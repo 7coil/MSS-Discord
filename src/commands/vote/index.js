11e1dda00e0a5e33483c1fdafe8dc77e5c865b17
lepon01 <leondrolio@gmail.com>
2017-05-27 13:11:59 +0100

    Add vote

---
+++ src/commands/vote/index.js
@@ -0,0 +1,26 @@
+const MSS = require("./../../functions/");
+
+module.exports = function yt(message) {
+	let input = message.content.replace (/\n/g, "").split(" ");
+
+	input.shift();
+	input.shift();
+	let items = input.join(" ");
+	let vote = items.split(";");
+
+	if(vote.length < 1) {
+		return message.channel.send("Too few options!");
+	} else if (vote.length > 9) {
+		return message.channel.send("Too many options!");
+	}
+
+	let reply = meta[message.data.lang] && meta[message.data.lang].message_choose_lang || "message_vote";
+
+	vote.forEach((item, iterator) => {
+		message.react(String.fromCodePoint(57884 + iterator))
+		reply += `\n${String.fromCodePoint(57884 + iterator)} ${item}`
+	});
+
+	message.channel.send(reply);
+}
+
