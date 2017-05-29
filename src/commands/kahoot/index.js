30d32a64702dba719af7d98118f6c98604add6ee
lepon01 <leondrolio@gmail.com>
2017-05-29 23:53:17 +0100

    Return stop

--- src/commands/kahoot/index.js
+++ src/commands/kahoot/index.js
@@ -6,7 +6,7 @@ const meta = require("./meta.json");
 module.exports = function help(message) {
 	let input = message.content.replace(/\n/g, "").split(" ");
 	if(!input[2]) return message.channel.send("No UUID/URL or command supplied.");
-	if(input[2] === "stop") MSS.kahoot.stop(message);
+	if(input[2] === "stop") return MSS.kahoot.stop(message);
 	let n = input[2].lastIndexOf('/');
 	let id = input[2].substring(n+1);
 	MSS.kahoot.add(message, id);
