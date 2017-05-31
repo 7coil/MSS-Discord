1437b6d4ea4fc16b03fa8ecfefe68bf08d6e701a
lepon01 <leondrolio@gmail.com>
2017-05-31 14:26:10 +0100

    Add meta to mod

--- src/commands/mod/index.js
+++ src/commands/mod/index.js
@@ -1,6 +1,6 @@
 module.exports = function(message) {
 	if (!message.guild) return message.channel.send("You are not in a guild!");
 	let moderator = message.guild.members.filter((user)=>{return user.hasPermission(0x00000008) && user.presence.status === "online"}).random() || message.guild.ownerID;
-	let report = message.words.shift().join(" ");
+	let report = message.words.shift().join(" ") || "No report provided :shrug:";
 	message.channel.send(`Report by <@${message.author.id}> to ${moderator}:\n${report}`);
 }
