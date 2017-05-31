ff762cf823dc78a59dab19b3abf4a6ffc79302d1
lepon01 <leondrolio@gmail.com>
2017-05-31 14:27:42 +0100

    shift

--- src/commands/mod/index.js
+++ src/commands/mod/index.js
@@ -1,6 +1,8 @@
 module.exports = function(message) {
 	if (!message.guild) return message.channel.send("You are not in a guild!");
 	let moderator = message.guild.members.filter((user)=>{return user.hasPermission(0x00000008) && user.presence.status === "online"}).random() || message.guild.ownerID;
-	let report = message.words.shift().join(" ") || "No report provided :shrug:";
+	message.words.shift();
+	message.words.shift();
+	let report = message.words.join(" ") || "No report provided :shrug:";
 	message.channel.send(`Report by <@${message.author.id}> to ${moderator}:\n${report}`);
 }
