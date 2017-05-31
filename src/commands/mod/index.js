b9fea49b65af4824ace3d41dec7438296300558c
lepon01 <leondrolio@gmail.com>
2017-05-31 16:24:14 +0100

    Actually mention now (danger!)

--- src/commands/mod/index.js
+++ src/commands/mod/index.js
@@ -5,14 +5,14 @@ module.exports = function(message) {
 	if (!message.guild) return message.channel.send("You are not in a guild!");
 	let moderator = message.guild.members.filter((user)=>{
 		return (user.hasPermission(0x00000008) || user.hasPermission(0x00000004) || user.hasPermission(0x00000002)) && user.presence.status === "online" && !user.bot
-	}).random().id || message.guild.ownerID;
+	}).random() || message.guild.owner;
 	message.words.shift();
 	message.words.shift();
 	let report = message.words.join(" ") || "No report provided :shrug:";
 
 	if(message.guild.id === "110373943822540800" && message.channel.id === "110373943822540800") {
 		return exec(command, function(error, stdout, stderr) {
-			message.channel.send(`Report by <@${message.author.id}> to ${moderator}:\n${report}\nAuto Screenshot:`, {
+			message.channel.send(`Report by ${message.author} to ${moderator}:\n${report}\nAuto Screenshot:`, {
 				files: [
 					"./screenshot.jpg"
 				]
