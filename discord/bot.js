ac7f4bfe78dace1d51a251712f16a60401955a14
lepon01 <leondrolio@gmail.com>
2017-06-01 23:45:12 +0100

    set game

--- discord/bot.js
+++ discord/bot.js
@@ -36,6 +36,8 @@ client.login(client.mss.api.discord);
 client.on("ready", function() {
 	console.log("Successfully connected to Discord!");
 
+	client.user.setGame("mss.ovh " + client.mss.mss.version);
+
 	//Push both types of mention to the allowed prefixes array
 	client.mss.config.prefix.push(`<@!${client.user.id}>`);
 
