85be3353f4ea021ff9f7d6fb9bbb36654ee9ecb2
lepon01 <leondrolio@gmail.com>
2017-06-03 15:08:01 +0100

    put +1 in the modulo later

--- discord/commands/rate/index.js
+++ discord/commands/rate/index.js
@@ -5,11 +5,11 @@ module.exports = function screenshot(message) {
 
 	if (!message.content) message.content = "The MSS help menu";
 
-	outof = parseInt(message.words[0], 10) + 1 || 11;
+	outof = parseInt(message.words[0], 10) || 10;
 
 	if (message.content.length > 100) message.content = message.content.substring(0,100) + "...";
 
-	let rank = parseInt(crypto.createHash("md5").update(message.content.toLowerCase()).digest("hex"), 16) % outof;
+	let rank = parseInt(crypto.createHash("md5").update(message.content.toLowerCase()).digest("hex"), 16) % (outof + 1);
 
 	message.channel.send(`I rate ${message.content} a ${rank} out of ${outof}`);
 
