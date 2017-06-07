8ae7d23d73f8302c5adeb796d87dc1937a748ad6
lepon01 <leondrolio@gmail.com>
2017-06-07 21:52:09 +0100

    use has for permissions

--- server/discord/functions/msg/isadmin.js
+++ server/discord/functions/msg/isadmin.js
@@ -2,7 +2,7 @@ const config = require('config');
 const react = require("./react");
 
 module.exports = function isadmin(message) {
-	if ((message.guild && (message.channel.permissionsFor(message.member).hasPermission("ADMINISTRATOR") || message.member.roles.some((role)=>role.name.toLowerCase().includes("admin")) || message.member.roles.some((role)=>role.name.toLowerCase().includes("mod"))) || config.get("admins").includes(message.author.id)) {
+	if ((message.guild &&(message.channel.permissionsFor(message.member).has(0x00000008) ||message.member.roles.some((role)=>role.name.toLowerCase().includes("admin")) ||message.member.roles.some((role)=>role.name.toLowerCase().includes("mod"))))|| config.get("admins").includes(message.author.id)) {
 		return true;
 	} else {
 		react(message, false, "x");
