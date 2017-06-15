5c55b9a57b063138bd447606cac609ba70e28a13
lepon01 <leondrolio@gmail.com>
2017-06-16 00:02:20 +0100

    use message.input instead

--- server/discord/cogs/rate/index.js
+++ server/discord/cogs/rate/index.js
@@ -7,7 +7,7 @@ module.exports.alias = [
 
 module.exports.command = function screenshot(message) {
 	if (!message.input) return message.channel.createMessage('I rate undefined a 11 out of 10');
-	if (message.input.length > 100) message.input = `${message.input.substring(0, 100)}...`;
-	const rank = parseInt(crypto.createHash('md5').update(message.content.toLowerCase()).digest('hex'), 16) % 11;
-	return message.channel.createMessage(`I rate ${message.content} a ${rank} out of 10`);
+	const thing = message.input.length < 100 ? message.input : `${message.input.substring(0, 100)}...`;
+	const rank = parseInt(crypto.createHash('md5').update(thing.toLowerCase()).digest('hex'), 16) % 11;
+	return message.channel.createMessage(`I rate ${thing} a ${rank} out of 10`);
 };
