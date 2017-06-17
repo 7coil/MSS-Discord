5ff5c9dd956388bf8b49727e5c45b3a638ac7cf8
lepon01 <leondrolio@gmail.com>
2017-06-17 16:51:59 +0100

    add undefined

--- server/discord/cogs/js/index.js
+++ server/discord/cogs/js/index.js
@@ -22,14 +22,17 @@ module.exports.command = (message) => {
 
 		try {
 			const output = eval(`(${message.input})`); // eslint-disable-line no-eval
-			if (output.length < 500) {
+			if (output && output.length > 500) {
 				embed.embed.fields.push({ name: 'Output', value: `\`\`\`\n${output}\n\`\`\`` });
 				return message.channel.createMessage(embed);
+			} else if (typeof output !== 'undefined') {
+				utils.gist(message.content, (url) => {
+					embed.embed.fields.push({ name: 'Output', value: `[GitHub Gist](${url})` });
+					return message.channel.createMessage(embed);
+				});
+			} else {
+				embed.embed.fields.push({ name: 'Output', value: 'Undefined' });
 			}
-			utils.gist(message.content, (url) => {
-				embed.embed.fields.push({ name: 'Output', value: `[GitHub Gist](${url})` });
-				return message.channel.createMessage(embed);
-			});
 		} catch (err) {
 			utils.gist(err.stack, (url) => {
 				embed.embed.fields.push({ name: 'Error', value: `[GitHub Gist](${url})` });
