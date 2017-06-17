f377529c36ddb147438a10e087f97f46c225d62d
lepon01 <leondrolio@gmail.com>
2017-06-17 16:48:32 +0100

    return send after the gisty gist

--- server/discord/cogs/js/index.js
+++ server/discord/cogs/js/index.js
@@ -24,16 +24,18 @@ module.exports.command = (message) => {
 			const output = eval(`(${message.input})`); // eslint-disable-line no-eval
 			if (output.length < 500) {
 				embed.embed.fields.push({ name: 'Output', value: `\`\`\`\n${output}\n\`\`\`` });
+				return message.channel.createMessage(embed);
 			}
 			utils.gist(message.content, (url) => {
 				embed.embed.fields.push({ name: 'Output', value: `[GitHub Gist](${url})` });
+				return message.channel.createMessage(embed);
 			});
 		} catch (err) {
 			utils.gist(err.stack, (url) => {
 				embed.embed.fields.push({ name: 'Error', value: `[GitHub Gist](${url})` });
+				return message.channel.createMessage(embed);
 			});
 		}
-		return message.channel.createMessage(embed);
 	}
 	return false;
 };
