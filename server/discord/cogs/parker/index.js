cd61d849f4b715bf53bbb2ce1d6f472e206a0935
lepon01 <leondrolio@gmail.com>
2017-06-18 22:13:57 +0100

    Add random parker images

---
+++ server/discord/cogs/parker/index.js
@@ -0,0 +1,37 @@
+const data = require('./data.json');
+
+const regex = /(\d+)/;
+
+function escapeChar(name) {
+	return name.replace(/\*/g, '\\*')
+		.replace(/_/g, '\\_')
+		.replace(/~/g, '\\~')
+		.replace(/`/g, '\\`');
+}
+
+module.exports.alias = [
+	'parker',
+	'matt',
+	'matt parker',
+	'square'
+];
+
+module.exports.command = (message, client) => {
+	const url = data.data[Math.floor(Math.random() * data.data.length)];
+	const id = regex.exec(message.input);
+	let sender = null;
+	let reciever = null;
+
+	if (message.author.id === id[1]) {
+		sender = 'yourself';
+		reciever = escapeChar(message.author.username);
+	} else if (client.users.get(id[1])) {
+		sender = escapeChar(message.author.username);
+		reciever = escapeChar(client.users.get(id[1]).username);
+	} else {
+		sender = escapeChar(message.author.username);
+		reciever = 'A user that doesnt exist';
+	}
+
+	message.channel.createMessage(`**${reciever}**, you got Parker Squared by **${sender}**!\n${url}`);
+};
