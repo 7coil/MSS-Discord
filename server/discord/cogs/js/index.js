53ac94a9183fbba3a5e8385f7f3ebaebea2207c6
lepon01 <leondrolio@gmail.com>
2017-06-18 22:57:02 +0100

    fix js output

--- server/discord/cogs/js/index.js
+++ server/discord/cogs/js/index.js
@@ -24,21 +24,21 @@ module.exports.command = (message) => {
 			const output = eval(`(${message.input})`); // eslint-disable-line no-eval
 			if (output && output.length > 500) {
 				embed.embed.fields.push({ name: 'Output', value: `\`\`\`\n${output}\n\`\`\`` });
-				return message.channel.createMessage(embed);
+				message.channel.createMessage(embed);
 			} else if (typeof output !== 'undefined') {
-				utils.gist(message.content, (url) => {
+				utils.gist(output, (url) => {
 					embed.embed.fields.push({ name: 'Output', value: `[GitHub Gist](${url})` });
-					return message.channel.createMessage(embed);
+					message.channel.createMessage(embed);
 				});
 			} else {
 				embed.embed.fields.push({ name: 'Output', value: 'Undefined' });
+				message.channel.createMessage(embed);
 			}
 		} catch (err) {
 			utils.gist(err.stack, (url) => {
 				embed.embed.fields.push({ name: 'Error', value: `[GitHub Gist](${url})` });
-				return message.channel.createMessage(embed);
+				message.channel.createMessage(embed);
 			});
 		}
 	}
-	return false;
 };
