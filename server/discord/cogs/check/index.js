c11f48281fbbc81c7fa88d0a5c5ba4040fce8a45
lepon01 <leondrolio@gmail.com>
2017-06-19 18:21:25 +0100

    ping discordapp.com

--- server/discord/cogs/check/index.js
+++ server/discord/cogs/check/index.js
@@ -4,4 +4,12 @@ module.exports.alias = [
 ];
 
 module.exports.command = message =>
-	message.channel.createMessage('Discord is currently `OFFLINE` and not working. Try using Skype while waiting for Discord to come back online?');
+	message.channel.createMessage(`\`\`\`
+PING discordapp.com (104.16.59.5) 56(84) bytes of data,
+
+--- 104.16.59.5 ping statistics ---
+4 packets transmitted, 0 recieved, 100% packet loss, time: 3001ms
+\`\`\`
+MSS has detected that Discord is offline.
+While you wait for Discord, give the MSN (Microsoft Network) a try.
+`);
