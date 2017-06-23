86c29d357890dc5c4fa08baa1530232edcb6b2f8
lepon01 <leondrolio@gmail.com>
2017-06-23 18:43:27 +0100

    author not user

--- server/discord/cogs/markov/index.js
+++ server/discord/cogs/markov/index.js
@@ -7,14 +7,14 @@ module.exports.alias = [
 module.exports.command = (message) => {
 	if (message.words[0] === 'register') {
 		if (message.words[1]) {
-			utils.markov.register(message.user.id, message.words[1], (reply) => {
+			utils.markov.register(message.author.id, message.words[1], (reply) => {
 				message.channel.createMessage(reply);
 			});
 		} else {
 			message.channel.createMessage('You did not supply a name. The name is what triggers the [whatever]pls Markov generator.');
 		}
 	} else if (message.words[0] === 'unregister') {
-		utils.markov.unregister(message.user.id, (reply) => {
+		utils.markov.unregister(message.author.id, (reply) => {
 			message.channel.createMessage(reply);
 		});
 	}
