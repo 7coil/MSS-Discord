9c030a0f429cfe6146c490382855bcb55b9fd964
lepon01 <leondrolio@gmail.com>
2017-06-26 21:54:22 +0100

    Add secret key to the MD5 process

--- server/discord/cogs/rate/index.js
+++ server/discord/cogs/rate/index.js
@@ -1,3 +1,4 @@
+const config = require('config');
 const crypto = require('crypto');
 
 module.exports.alias = [
@@ -9,6 +10,6 @@ module.exports.command = function screenshot(message) {
 	if (!message.input) return message.channel.createMessage(`I ${message.command} undefined an 11 out of 10`);
 	const input = message.input.replace(/‌/g, '');
 	const thing = message.input.length < 100 ? input : `${input.substring(0, 100)}...`;
-	const rank = parseInt(crypto.createHash('md5').update(thing.toLowerCase()).digest('hex'), 16) % 11;
+	const rank = parseInt(crypto.createHash('md5').update(thing.toLowerCase() + config.get('session').secret).digest('hex'), 16) % 11;
 	return message.channel.createMessage(`I ${message.command} ${thing} a ${rank} out of 10`);
 };
