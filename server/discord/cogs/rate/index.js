92f8800fd7ba4a9b68018137f7c84190e4694b31
lepon01 <leondrolio@gmail.com>
2017-06-26 21:51:34 +0100

    replace with nothing

--- server/discord/cogs/rate/index.js
+++ server/discord/cogs/rate/index.js
@@ -7,7 +7,7 @@ module.exports.alias = [
 
 module.exports.command = function screenshot(message) {
 	if (!message.input) return message.channel.createMessage(`I ${message.command} undefined an 11 out of 10`);
-	const input = message.input.replace(/‌/g);
+	const input = message.input.replace(/‌/g, '');
 	const thing = message.input.length < 100 ? input : `${input.substring(0, 100)}...`;
 	const rank = parseInt(crypto.createHash('md5').update(thing.toLowerCase()).digest('hex'), 16) % 11;
 	return message.channel.createMessage(`I ${message.command} ${thing} a ${rank} out of 10`);
