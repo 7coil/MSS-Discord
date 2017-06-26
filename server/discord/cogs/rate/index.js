fba5634492aa2aa5b8b03accda84527e35a16f5b
lepon01 <leondrolio@gmail.com>
2017-06-26 21:50:49 +0100

    Remove zero width joining thing

--- server/discord/cogs/rate/index.js
+++ server/discord/cogs/rate/index.js
@@ -6,8 +6,9 @@ module.exports.alias = [
 ];
 
 module.exports.command = function screenshot(message) {
-	if (!message.input) return message.channel.createMessage('I rate undefined a 11 out of 10');
-	const thing = message.input.length < 100 ? message.input : `${message.input.substring(0, 100)}...`;
+	if (!message.input) return message.channel.createMessage(`I ${message.command} undefined an 11 out of 10`);
+	const input = message.input.replace(/‌/g);
+	const thing = message.input.length < 100 ? input : `${input.substring(0, 100)}...`;
 	const rank = parseInt(crypto.createHash('md5').update(thing.toLowerCase()).digest('hex'), 16) % 11;
-	return message.channel.createMessage(`I rate ${thing} a ${rank} out of 10`);
+	return message.channel.createMessage(`I ${message.command} ${thing} a ${rank} out of 10`);
 };
