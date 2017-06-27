29d149d1003615a7c6585d66bf14b34ee95ee49c
lepon01 <leondrolio@gmail.com>
2017-06-27 15:09:52 +0100

    change stdout to stderr

--- server/discord/cogs/exec/index.js
+++ server/discord/cogs/exec/index.js
@@ -10,28 +10,28 @@ module.exports.command = (message) => {
 	if (config.get('admins').includes(message.author.id)) {
 		if (!message.content) {
 			message.channel.send('No input detected');
-			return false;
+		} else {
+			exec(message.content, (error, stdout, stderr) => {
+				let output = '';
+
+				if (stdout) {
+					output += '=== stdout ===\n';
+					output += `${stdout.replace(/`/g, '\'')}\n`;
+				}
+
+				if (stderr) {
+					output += '=== stderr ===\n';
+					output += `${stderr.replace(/`/g, '\'')}\n`;
+				}
+
+				if (output && output.length > 1900) {
+					message.client.mss.functions.system.gist(message, (url) => {
+						message.channel.createMessage(`[GitHub](${url})`);
+					});
+				} else {
+					message.channel.createMessage(`\nOutput\n\`\`\`\n${output}\`\`\``);
+				}
+			});
 		}
-
-		exec(message.content, (error, stdout, stderr) => {
-			let output = '';
-
-			if (stdout) {
-				output += `${stdout.replace(/`/g, '\'')}\n`;
-			}
-
-			if (stderr) {
-				output += `${stdout.replace(/`/g, '\'')}\n`;
-			}
-
-			if (output && output.length > 1900) {
-				message.client.mss.functions.system.gist(message, (url) => {
-					message.channel.createMessage(`[GitHub](${url})`);
-				});
-			} else {
-				message.channel.createMessage(`\nOutput\n\`\`\`\n${output}\`\`\``);
-			}
-		});
 	}
-	return false;
 };
