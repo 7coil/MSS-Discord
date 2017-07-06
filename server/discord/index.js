e85349270f53068971e192cde72e356dfbdf8dd2
lepon01 <leondrolio@gmail.com>
2017-07-06 21:20:31 +0100

    put inside client ready

--- server/discord/index.js
+++ server/discord/index.js
@@ -54,59 +54,59 @@ client.on('ready', () => {
 	setInterval(() => {
 		utils.collection.check(client);
 	}, 1800000);
-});
 
-client.on('messageCreate', (message) => {
-	// It crashed on this before. No explaination.
-	if (!message.author) return;
-	// Disallow if the author is a bot
-	if (message.author.bot) return;
+	client.on('messageCreate', (message) => {
+		// It crashed on this before. No explaination.
+		if (!message.author) return;
+		// Disallow if the author is a bot
+		if (message.author.bot) return;
 
-	// Test the message content on the regular expression for prefixed commands and the suffixed commands
-	const pre = prefix.exec(message.content);
-	const suf = suffix.exec(message.content);
+		// Test the message content on the regular expression for prefixed commands and the suffixed commands
+		const pre = prefix.exec(message.content);
+		const suf = suffix.exec(message.content);
 
-	// If there's a result, do this crap.
-	if (pre) {
-		// Bake some cool extra crap into the message
-		message.prefix = pre[1];
-		message.command = pre[2];
-		message.input = pre[3] || null;
-		message.words = pre[3].split(/\n+|\s+/g);
+		// If there's a result, do this crap.
+		if (pre) {
+			// Bake some cool extra crap into the message
+			message.prefix = pre[1];
+			message.command = pre[2];
+			message.input = pre[3] || null;
+			message.words = pre[3].split(/\n+|\s+/g);
 
-		// Run the actual command, if the command exists
-		commands[message.command].command(message, client);
-	} else if (suf) {
-		if (message.channel && message.channel.id !== '110373943822540800') {
-			// If the suffix matches a user's name in the database...
+			// Run the actual command, if the command exists
+			commands[message.command].command(message, client);
+		} else if (suf) {
+			if (message.channel && message.channel.id !== '110373943822540800') {
+				// If the suffix matches a user's name in the database...
+				r.table('markov')
+					.filter({ name: suf[1] })
+					.run(r.conn, (err1, cursor) => {
+						if (err1) return;
+						cursor.toArray((err2, result) => {
+							if (err2) return;
+							if (result.length === 0) return;
+							// Generate a Markov
+							utils.markov.generate(suf[1], (embed) => {
+								message.channel.createMessage(embed);
+							});
+						});
+					});
+			}
+		} else {
+			// If the user exists in the rethonk Markov database...
 			r.table('markov')
-				.filter({ name: suf[1] })
+				.filter({ author: message.author.id })
 				.run(r.conn, (err1, cursor) => {
 					if (err1) return;
 					cursor.toArray((err2, result) => {
 						if (err2) return;
 						if (result.length === 0) return;
-						// Generate a Markov
-						utils.markov.generate(suf[1], (embed) => {
-							message.channel.createMessage(embed);
-						});
+						// Save the markov.
+						utils.markov.save(message);
 					});
 				});
 		}
-	} else {
-		// If the user exists in the rethonk Markov database...
-		r.table('markov')
-			.filter({ author: message.author.id })
-			.run(r.conn, (err1, cursor) => {
-				if (err1) return;
-				cursor.toArray((err2, result) => {
-					if (err2) return;
-					if (result.length === 0) return;
-					// Save the markov.
-					utils.markov.save(message);
-				});
-			});
-	}
+	});
 });
 
 client.connect();
