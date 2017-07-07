5cf92ed8301e3b87b2531f291fef55b00e09f785
lepon01 <leondrolio@gmail.com>
2017-07-07 15:50:16 +0100

    prune?

--- server/discord/cogs/clean/index.js
+++ server/discord/cogs/clean/index.js
@@ -1,7 +1,10 @@
 const client = require('./../../');
+const utils = require('./../../utils.js');
+
+const regex = /\w+/;
 
 module.exports.info = {
-	name: 'Clean messages',
+	name: 'Clean messages by MSS',
 	category: 'utilities',
 	aliases: [
 		'clean',
@@ -10,11 +13,21 @@ module.exports.info = {
 };
 
 module.exports.command = (message) => {
-	message.channel.getMessages().then((messages) => {
-		messages.filter(msg =>
-			msg.author.id === client.user.id
-		).forEach((msg) => {
-			msg.delete();
+	if (utils.isadmin(message.member)) {
+		const prune = message.input ? regex.exec(message.input) : client.user.id;
+		message.channel.getMessages().then((messages) => {
+			messages.filter((msg) => {
+				if (prune === 'bots') {
+					return msg.author.bot;
+				}
+				return msg.author.id === prune;
+			}).forEach((msg) => {
+				msg.delete().catch(() => {
+					console.log('Insufficient permissions (probably)');
+				});
+			});
 		});
-	});
+	} else {
+		message.channel.createMessage('You do not have permission to perform this command!');
+	}
 };
