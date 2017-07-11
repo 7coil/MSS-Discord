b1c4ccd5071e3cf046131608661f72f006aa6417
lepon01 <leondrolio@gmail.com>
2017-07-11 17:55:37 +0100

    stop mod out of 2

--- server/discord/cogs/rate/index.js
+++ server/discord/cogs/rate/index.js
@@ -24,7 +24,7 @@ module.exports.command = (message) => {
 	if (message.input) {
 		const input = message.input.replace(/\W/g, '');
 		const hash = crypto.createHash('md5').update(input + config.get('secret')).digest('hex').substring(0, 4);
-		const rank = parseInt(hash, 16) % 2;
+		const rank = parseInt(hash, 16) % 11;
 		const thing = message.input.length < 100 ? input : `${input.substring(0, 100)}...`;
 		message.channel.createMessage(`I ${message.command} ${thing} a ${rank} out of 10`);
 	}
