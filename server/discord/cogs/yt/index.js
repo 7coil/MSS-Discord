7e03706308a727b76879a17b04d18218ad7c02c1
lepon01 <leondrolio@gmail.com>
2017-07-11 18:12:01 +0100

    no ytdl-core

--- server/discord/cogs/yt/index.js
+++ server/discord/cogs/yt/index.js
@@ -34,18 +34,6 @@ module.exports.command = (message) => {
 					message.channel.createMessage(error);
 				} else if (!video) {
 					message.channel.createMessage('No results found');
-				} else if (video.snippet.liveBroadcastContent === 'live') {
-					utils.music.add(message, {
-						type: 'ytdl-core',
-						from: 'YouTube',
-						media: {
-							url: `https://youtube.com/watch?v=${video.id.videoId}`,
-							search: { quality: ['96', '95', '94', '93', '92', '91'] }
-						},
-						title: video.snippet.title,
-						thumb: video.snippet.thumbnails.default.url,
-						desc: video.snippet.description
-					});
 				} else {
 					utils.music.add(message, {
 						type: 'youtube-dl',
