c69164b0bb30db0688759bfe6263df479de609f8
lepon01 <leondrolio@gmail.com>
2017-07-13 14:21:18 +0100

    Split Markov command

---
+++ server/discord/cogs/mkregister/index.js
@@ -0,0 +1,26 @@
+const utils = require('./../../utils.js');
+
+module.exports.info = {
+	name: 'Register from Markov',
+	description: 'Register for Markov based messaging. Stores messages you send, but in such a way that only analytics about the data is stores, so complies with EU and UK data protection policies.',
+	category: 'Fun',
+	aliases: [
+		'markov'
+	],
+	use: [
+		{
+			name: '',
+			value: 'Register for the Markov machine learning service'
+		}
+	]
+};
+
+module.exports.command = (message) => {
+	if (message.words[1]) {
+		utils.markov.register(message.author.id, message.words[1], (reply) => {
+			message.channel.createMessage(reply);
+		});
+	} else {
+		message.channel.createMessage('You did not supply a name. The name is what triggers the [whatever]pls Markov generator.');
+	}
+};
---
+++ server/discord/cogs/mkunregister/index.js
@@ -0,0 +1,25 @@
+const utils = require('./../../utils.js');
+
+module.exports.info = {
+	name: 'Un-register from Markov',
+	description: 'Un-register for Markov based messaging.',
+	category: 'Fun',
+	aliases: [
+		'markov'
+	],
+	use: [
+		{
+			name: 'register',
+			value: 'Register for the Markov machine learning service'
+		}, {
+			name: 'unregister',
+			value: 'Remove yourself and your data from the Markov machine learning service'
+		}
+	]
+};
+
+module.exports.command = (message) => {
+	utils.markov.unregister(message.author.id, (reply) => {
+		message.channel.createMessage(reply);
+	});
+};
